4453d80f1357d355567d2bd31f8e97fe606820a8
Richard Hughes <hughsie@hughsie-laptop.(none)>
2007-08-13 15:07:21 +0100

    remove job-list from task-client

--- client/pk-task-client.c
+++ client/pk-task-client.c
@@ -54,7 +54,6 @@ struct PkTaskClientPrivate
 };
 
 typedef enum {
-	PK_TASK_CLIENT_JOB_LIST_CHANGED,
 	PK_TASK_CLIENT_JOB_STATUS_CHANGED,
 	PK_TASK_CLIENT_PERCENTAGE_CHANGED,
 	PK_TASK_CLIENT_PACKAGE,
@@ -530,11 +529,6 @@ pk_task_client_class_init (PkTaskClientClass *klass)
 
 	object_class->finalize = pk_task_client_finalize;
 
-	signals [PK_TASK_CLIENT_JOB_LIST_CHANGED] =
-		g_signal_new ("job-list-changed",
-			      G_TYPE_FROM_CLASS (object_class), G_SIGNAL_RUN_LAST,
-			      0, NULL, NULL, g_cclosure_marshal_VOID__BOXED,
-			      G_TYPE_NONE, 1, dbus_g_type_get_collection ("GArray", G_TYPE_UINT));
 	signals [PK_TASK_CLIENT_JOB_STATUS_CHANGED] =
 		g_signal_new ("job-status-changed",
 			      G_TYPE_FROM_CLASS (object_class), G_SIGNAL_RUN_LAST,
