e2348cca93134a91949ba50aeebad6469a40266f
Ken VanDine <ken@vandine.org>
2007-08-24 18:49:58 +0100

    better conary queries

--- helpers/conary-search-name.py
+++ helpers/conary-search-name.py
@@ -26,7 +26,7 @@ affinityDb = client.db
 options = sys.argv[1]
 searchterms = sys.argv[2]
 
-sys.stderr.write('no-percentage-updates')
+sys.stderr.write('no-percentage-updates\n')
 
 try:
     localInstall = db.findTrove(None, (searchterms, None, None))
@@ -37,16 +37,36 @@ except:
 troveSpecs = [ cmdline.parseTroveSpec(searchterms, allowEmptyName=False)]
 
 try:
-    #Not sure which way is better, findTroves or getTrovesToDisplay
-    #troveTupleList = repos.findTroves(cfg.installLabelPath, troveSpecs, cfg.flavor, getLeaves=True, acrossFlavors=False, acrossLabels=False, bestFlavor=True, affinityDatabase=affinityDb )
-    troveTupleList = queryrep.getTrovesToDisplay(repos, troveSpecs, None, None, queryrep.VERSION_FILTER_LEAVES, queryrep.FLAVOR_FILTER_BEST, cfg.installLabelPath, cfg.flavor, affinityDb)
+    # Look for packages with affinity
+    troveTupleList = queryrep.getTrovesToDisplay(repos, troveSpecs,
+        None, None, queryrep.VERSION_FILTER_LATEST,
+        queryrep.FLAVOR_FILTER_BEST, cfg.installLabelPath,
+        cfg.flavor, affinityDb)
+    # Look for packages regardless of affinity
+    troveTupleList.extend(queryrep.getTrovesToDisplay(repos, troveSpecs,
+        None, None, queryrep.VERSION_FILTER_LATEST,
+        queryrep.FLAVOR_FILTER_BEST, cfg.installLabelPath,
+        cfg.flavor, None))
+    # Remove dupes
+    tempDict = {}
+    for element in troveTupleList:
+        tempDict[element] = None
+        troveTupleList = tempDict.keys()
+
+    # Get the latest first
+    troveTupleList.sort()
+    troveTupleList.reverse()
+
     for troveTuple in troveTupleList:
         name = troveTuple[0]
         version = troveTuple[1].trailingRevision().asString()
-        arch = ""
+        # Hard code this until i get the flavor parsing right
+        arch = "x86"
         fullVersion = troveTuple[1].asString()
         flavor = str(troveTuple[2])
         data = fullVersion + " " + flavor
+        # We don't have summary data yet... so leave it blank for now
+        summary = " "
         package_id = name + ";" + version + ";" + arch + ";" + data
         do_print = 0;
         if options == 'installed' and installed == 1:
@@ -57,6 +77,6 @@ try:
             do_print = 1
         # print in correct format
         if do_print == 1:
-            print "package\t%s\t%s\t%s" % (installed, package_id, "")
+            print "package\t%s\t%s\t%s" % (installed, package_id, summary)
 except:
-    pass
+    sys.stderr.write('error\tinternal-error\tAn internal error has occurred')
