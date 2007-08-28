ab855bbf92cefeb4743935e9a373ccb247106cfb
Tom Parker <palfrey@tevp.net>
2007-08-28 19:29:50 +0100

    Refresh cache and not_implemented changes

--- src/pk-task-apt.cpp
+++ src/pk-task-apt.cpp
@@ -28,6 +28,9 @@
 #include <apt-pkg/init.h>
 #include <apt-pkg/pkgrecords.h>
 #include <apt-pkg/sourcelist.h>
+#include <apt-pkg/error.h>
+#include <apt-pkg/acquire.h>
+#include <apt-pkg/acquire-item.h>
 
 #include <regex.h>
 #include <string.h>
@@ -35,8 +38,8 @@
 #include "pk-debug.h"
 #include "pk-task.h"
 #include "pk-task-common.h"
-#include "pk-spawn.h"
 #include "config.h"
+#include "pk-network.h"
 
 static void pk_task_class_init(PkTaskClass * klass);
 static void pk_task_init(PkTask * task);
@@ -46,23 +49,24 @@ pkgSourceList *SrcList = 0;
 
 #define PK_TASK_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), PK_TYPE_TASK, PkTaskPrivate))
 
-static pkgCacheFile* getCache()
+static pkgCacheFile *fileCache = NULL;
+
+static pkgCacheFile *getCache()
 {
-	static pkgCacheFile *fileCache = NULL;
 	if (fileCache == NULL)
 	{
-      	MMap *Map = 0;
+		MMap *Map = 0;
 		OpTextProgress Prog;
 		if (pkgInitConfig(*_config) == false)
 			pk_debug("pkginitconfig was false");
 		if (pkgInitSystem(*_config, _system) == false)
 			pk_debug("pkginitsystem was false");
-		 // Open the cache file
-		 SrcList = new pkgSourceList;
-		 SrcList->ReadMainList();
+		// Open the cache file
+		SrcList = new pkgSourceList;
+		SrcList->ReadMainList();
 
-		 // Generate it and map it
-		 pkgMakeStatusCache(*SrcList,Prog,&Map,true);
+		// Generate it and map it
+		pkgMakeStatusCache(*SrcList, Prog, &Map, true);
 
 		fileCache = new pkgCacheFile();
 
@@ -81,12 +85,12 @@ static pkgCacheFile* getCache()
 struct PkTaskPrivate
 {
 	guint progress_percentage;
+	PkNetwork *network;
 };
 
 static guint signals[PK_TASK_LAST_SIGNAL] = { 0, };
 
 G_DEFINE_TYPE(PkTask, pk_task, G_TYPE_OBJECT)
-
 /**
  * pk_task_get_updates:
  **/
@@ -100,8 +104,137 @@ gboolean pk_task_get_updates(PkTask * task)
 		return FALSE;
 	}
 
-	/* not implimented yet */
+ 	pk_task_not_implemented_yet(task, "GetUpdates");
 	return FALSE;
+}
+
+typedef struct
+{
+	PkTask *task;
+} UpdateData;
+
+class UpdatePercentage:public pkgAcquireStatus
+{
+	virtual bool MediaChange(string Media,string Drive)
+	{
+		pk_debug("PANIC!: we don't handle mediachange");
+		return FALSE;
+	}
+	
+	virtual bool Pulse(pkgAcquire *Owner)
+	{
+		return true;	
+	}
+};
+
+// DoUpdate - Update the package lists 
+// Swiped from apt-get's update mode
+void *DoUpdate(gpointer data)
+{
+	UpdateData *ud = (UpdateData*)data;
+	pkgCacheFile *Cache;
+	bool Failed = false;
+	bool TransientNetworkFailure = false;
+	OpTextProgress Prog;
+	
+	/* easy as that */
+	pk_task_change_job_status(ud->task, PK_TASK_STATUS_REFRESH_CACHE);
+
+	Cache = getCache();
+
+	// Get the source list
+	pkgSourceList List;
+	if (List.ReadMainList() == false)
+	{
+		pk_task_error_code(ud->task, PK_TASK_ERROR_CODE_UNKNOWN, "Failure reading lists");
+		pk_task_finished(ud->task, PK_TASK_EXIT_FAILED);
+		return NULL;
+	}
+
+	// Lock the list directory
+	FileFd Lock;
+	if (_config->FindB("Debug::NoLocking", false) == false)
+	{
+		Lock.Fd(GetLock(_config->FindDir("Dir::State::Lists") + "lock"));
+		if (_error->PendingError() == true)
+		{
+		    _error->DumpErrors();
+			pk_task_error_code(ud->task, PK_TASK_ERROR_CODE_UNKNOWN, "Unable to lock the list directory");
+			pk_task_finished(ud->task, PK_TASK_EXIT_FAILED);
+			return NULL;
+		}
+	}
+
+	// Create the download object
+	UpdatePercentage *Stat = new UpdatePercentage();
+	pkgAcquire Fetcher(Stat);
+
+	// Populate it with the source selection
+	if (List.GetIndexes(&Fetcher) == false)
+	{
+		pk_task_error_code(ud->task, PK_TASK_ERROR_CODE_UNKNOWN, "Generic Error");
+		goto do_update_clean;
+	}
+
+	// Run it
+	if (Fetcher.Run() == pkgAcquire::Failed)
+	{
+		pk_task_error_code(ud->task, PK_TASK_ERROR_CODE_UNKNOWN, "Generic Error");
+		goto do_update_clean;
+	}
+
+	for (pkgAcquire::ItemIterator I = Fetcher.ItemsBegin(); I != Fetcher.ItemsEnd(); I++)
+	{
+		if ((*I)->Status == pkgAcquire::Item::StatDone)
+			continue;
+
+		(*I)->Finished();
+
+		fprintf(stderr, "Failed to fetch %s  %s\n", (*I)->DescURI().c_str(), (*I)->ErrorText.c_str());
+
+		if ((*I)->Status == pkgAcquire::Item::StatTransientNetworkError)
+		{
+			TransientNetworkFailure = true;
+			continue;
+		}
+
+		Failed = true;
+	}
+
+	// Clean out any old list files
+	if (!TransientNetworkFailure && _config->FindB("APT::Get::List-Cleanup", true) == true)
+	{
+		if (Fetcher.Clean(_config->FindDir("Dir::State::lists")) == false || Fetcher.Clean(_config->FindDir("Dir::State::lists") + "partial/") == false)
+		{
+			pk_task_error_code(ud->task, PK_TASK_ERROR_CODE_UNKNOWN, "Generic Error");
+			goto do_update_clean;
+		}
+	}
+
+	// Prepare the cache.   
+	Cache = getCache();
+	if (Cache->BuildCaches(Prog,false) == false)
+	{
+		pk_task_error_code(ud->task, PK_TASK_ERROR_CODE_UNKNOWN, "Generic Error");
+		goto do_update_clean;
+	}
+
+	if (TransientNetworkFailure == true)
+		pk_debug("Some index files failed to download, they have been ignored, or old ones used instead.");
+	else if (Failed == true)
+	{
+		pk_task_error_code(ud->task, PK_TASK_ERROR_CODE_UNKNOWN, "Generic Error");
+		goto do_update_clean;
+	}
+
+	delete Stat;
+	pk_task_finished(ud->task, PK_TASK_EXIT_SUCCESS);
+	return NULL;
+
+	do_update_clean:
+	delete Stat;
+	pk_task_finished(ud->task, PK_TASK_EXIT_FAILED);
+	return NULL;
 }
 
 /**
@@ -117,8 +250,30 @@ gboolean pk_task_refresh_cache(PkTask * task, gboolean force)
 		return FALSE;
 	}
 
-	/* not implimented yet */
-	return FALSE;
+	/* check network state */
+	if (pk_network_is_online(task->priv->network) == FALSE)
+	{
+		pk_task_error_code(task, PK_TASK_ERROR_CODE_NO_NETWORK, "Cannot refresh cache whilst offline");
+		pk_task_finished(task, PK_TASK_EXIT_FAILED);
+		return TRUE;
+	}
+
+	UpdateData *data = g_new(UpdateData, 1);
+	if (data == NULL)
+	{
+		pk_task_error_code(task, PK_TASK_ERROR_CODE_UNKNOWN, "can't allocate memory for update task");
+		pk_task_finished(task, PK_TASK_EXIT_FAILED);
+	}
+	else
+	{
+		data->task = task;
+		if (g_thread_create(DoUpdate,data, false, NULL) == NULL)
+		{
+			pk_task_error_code(task, PK_TASK_ERROR_CODE_UNKNOWN, "can't spawn update thread");
+			pk_task_finished(task, PK_TASK_EXIT_FAILED);
+		}
+	}
+	return TRUE;
 }
 
 /**
@@ -228,9 +383,11 @@ static gboolean buildExDesc(ExDescFile *DFList, unsigned int pid, pkgCache::VerI
 	return DFList[pid].available;
 }
 
-static void * do_search_task(gpointer data)
+// do_search_task
+// Swiped from apt-cache's search mode
+static void *do_search_task(gpointer data)
 {
-	search_task *st = (search_task*)data;
+	search_task *st = (search_task *) data;
 	ExDescFile *DFList = NULL;
 
 	pk_task_change_job_status(st->task, PK_TASK_STATUS_QUERY);
@@ -247,8 +404,8 @@ static void * do_search_task(gpointer data)
 	memset(Pattern, 0, sizeof(*Pattern));
 	if (regcomp(Pattern, st->search, REG_EXTENDED | REG_ICASE | REG_NOSUB) != 0)
 	{
-		pk_task_error_code (st->task, PK_TASK_ERROR_CODE_UNKNOWN, "regex compilation error");
-		pk_task_finished (st->task, PK_TASK_EXIT_FAILED);
+		pk_task_error_code(st->task, PK_TASK_ERROR_CODE_UNKNOWN, "regex compilation error");
+		pk_task_finished(st->task, PK_TASK_EXIT_FAILED);
 		goto search_task_cleanup;
 	}
 
@@ -270,7 +427,7 @@ static void * do_search_task(gpointer data)
 
 		// Find the proper version to use. 
 		pkgCache::VerIterator V = Plcy.GetCandidateVer(P);
-		buildExDesc(DFList,P->ID,V);
+		buildExDesc(DFList, P->ID, V);
 	}
 
 	// Include all the packages that provide matching names too
@@ -282,7 +439,7 @@ static void * do_search_task(gpointer data)
 		for (pkgCache::PrvIterator Prv = P.ProvidesList(); Prv.end() == false; Prv++)
 		{
 			pkgCache::VerIterator V = Plcy.GetCandidateVer(Prv.OwnerPkg());
-			if (buildExDesc(DFList,Prv.OwnerPkg()->ID,V))
+			if (buildExDesc(DFList, Prv.OwnerPkg()->ID, V))
 				DFList[Prv.OwnerPkg()->ID].NameMatch = true;
 		}
 	}
@@ -292,11 +449,11 @@ static void * do_search_task(gpointer data)
 	// Iterate over all the version records and check them
 	for (ExDescFile * J = DFList; J->Df != 0; J++)
 	{
-   		#ifdef APT_PKG_RPM
+#ifdef APT_PKG_RPM
 		pkgRecords::Parser & P = Recs.Lookup(pkgCache::VerFileIterator(pkgCache, J->Df));
-		#else
+#else
 		pkgRecords::Parser & P = Recs.Lookup(pkgCache::DescFileIterator(pkgCache, J->Df));
-		#endif
+#endif
 
 		gboolean Match = true;
 		if (J->NameMatch == false)
@@ -318,12 +475,12 @@ static void * do_search_task(gpointer data)
 
 	pk_task_finished(st->task, PK_TASK_EXIT_SUCCESS);
 
-	search_task_cleanup:
+  search_task_cleanup:
 	for (ExDescFile * J = DFList; J->Df != 0; J++)
 	{
-		g_free(J->repo);	
+		g_free(J->repo);
 	}
-	delete[] DFList;
+	delete[]DFList;
 	regfree(Pattern);
 	g_free(st->search);
 	g_free(st);
@@ -335,7 +492,7 @@ static void * do_search_task(gpointer data)
  * pk_task_search
  **/
 static gboolean
-pk_task_search (PkTask *task, const gchar *filter, const gchar *search, SearchDepth which)
+pk_task_search(PkTask * task, const gchar * filter, const gchar * search, SearchDepth which)
 {
 	g_return_val_if_fail(task != NULL, FALSE);
 	g_return_val_if_fail(PK_IS_TASK(task), FALSE);
@@ -345,17 +502,18 @@ pk_task_search (PkTask *task, const gchar *filter, const gchar *search, SearchDe
 		return FALSE;
 	}
 
-	if (pk_task_check_filter (filter) == FALSE) {
-		pk_task_error_code (task, PK_TASK_ERROR_CODE_FILTER_INVALID, "filter '%s' not valid", filter);
-		pk_task_finished (task, PK_TASK_EXIT_FAILED);
+	if (pk_task_check_filter(filter) == FALSE)
+	{
+		pk_task_error_code(task, PK_TASK_ERROR_CODE_FILTER_INVALID, "filter '%s' not valid", filter);
+		pk_task_finished(task, PK_TASK_EXIT_FAILED);
 		return TRUE;
 	}
 
 	search_task *data = g_new(struct search_task, 1);
 	if (data == NULL)
 	{
-		pk_task_error_code (task, PK_TASK_ERROR_CODE_UNKNOWN, "can't allocate memory for search task");
-		pk_task_finished (task, PK_TASK_EXIT_FAILED);
+		pk_task_error_code(task, PK_TASK_ERROR_CODE_UNKNOWN, "can't allocate memory for search task");
+		pk_task_finished(task, PK_TASK_EXIT_FAILED);
 	}
 	else
 	{
@@ -364,10 +522,10 @@ pk_task_search (PkTask *task, const gchar *filter, const gchar *search, SearchDe
 		data->filter = g_strdup(filter);
 		data->depth = which;
 
-		if (g_thread_create(do_search_task,data,false,NULL)==NULL)
+		if (g_thread_create(do_search_task, data, false, NULL) == NULL)
 		{
-			pk_task_error_code (task, PK_TASK_ERROR_CODE_UNKNOWN, "can't spawn thread");
-			pk_task_finished (task, PK_TASK_EXIT_FAILED);
+			pk_task_error_code(task, PK_TASK_ERROR_CODE_UNKNOWN, "can't spawn thread");
+			pk_task_finished(task, PK_TASK_EXIT_FAILED);
 		}
 	}
 	return TRUE;
@@ -376,38 +534,34 @@ pk_task_search (PkTask *task, const gchar *filter, const gchar *search, SearchDe
 /**
  * pk_task_search_details:
  **/
-gboolean
-pk_task_search_details (PkTask *task, const gchar *filter, const gchar *search)
+gboolean pk_task_search_details(PkTask * task, const gchar * filter, const gchar * search)
 {
-	return pk_task_search(task,filter,search,SEARCH_DETAILS);
+	return pk_task_search(task, filter, search, SEARCH_DETAILS);
 }
 
 /**
  * pk_task_search_name:
  **/
-gboolean
-pk_task_search_name (PkTask *task, const gchar *filter, const gchar *search)
+gboolean pk_task_search_name(PkTask * task, const gchar * filter, const gchar * search)
 {
-	return pk_task_search(task,filter,search,SEARCH_NAME);
+	return pk_task_search(task, filter, search, SEARCH_NAME);
 }
 
 /**
  * pk_task_search_group:
  **/
-gboolean
-pk_task_search_group (PkTask *task, const gchar *filter, const gchar *search)
+gboolean pk_task_search_group(PkTask * task, const gchar * filter, const gchar * search)
 {
-	pk_task_not_implemented_yet (task, "SearchGroup");
+	pk_task_not_implemented_yet(task, "SearchGroup");
 	return TRUE;
 }
 
 /**
  * pk_task_search_file:
  **/
-gboolean
-pk_task_search_file (PkTask *task, const gchar *filter, const gchar *search)
+gboolean pk_task_search_file(PkTask * task, const gchar * filter, const gchar * search)
 {
-	pk_task_not_implemented_yet (task, "SearchFile");
+	pk_task_not_implemented_yet(task, "SearchFile");
 	return TRUE;
 }
 
@@ -424,7 +578,7 @@ gboolean pk_task_get_deps(PkTask * task, const gchar * package)
 		return FALSE;
 	}
 
-	/* not implimented yet */
+ 	pk_task_not_implemented_yet (task, "GetDeps");
 	return FALSE;
 }
 
@@ -441,7 +595,7 @@ gboolean pk_task_get_description(PkTask * task, const gchar * package)
 		return FALSE;
 	}
 
-	/* not implimented yet */
+ 	pk_task_not_implemented_yet (task, "GetDescription");
 	return FALSE;
 }
 
@@ -458,7 +612,7 @@ gboolean pk_task_remove_package(PkTask * task, const gchar * package, gboolean a
 		return FALSE;
 	}
 
-	/* not implimented yet */
+ 	pk_task_not_implemented_yet(task, "RemovePackage");
 	return FALSE;
 }
 
@@ -475,7 +629,7 @@ gboolean pk_task_install_package(PkTask * task, const gchar * package)
 		return FALSE;
 	}
 
-	/* not implimented yet */
+ 	pk_task_not_implemented_yet(task, "InstallPackage");
 	return FALSE;
 }
 
@@ -494,7 +648,7 @@ gboolean pk_task_cancel_job_try(PkTask * task)
 		return FALSE;
 	}
 
-	/* not implimented yet */
+ 	pk_task_not_implemented_yet (task, "CancelJobTry");
 	return FALSE;
 }
 
@@ -533,6 +687,7 @@ static void pk_task_class_init(PkTaskClass * klass)
 static void pk_task_init(PkTask * task)
 {
 	task->priv = PK_TASK_GET_PRIVATE(task);
+	task->priv->network = pk_network_new();
 	task->signals = signals;
 	pk_task_clear(task);
 }
@@ -548,6 +703,7 @@ static void pk_task_finalize(GObject * object)
 	task = PK_TASK(object);
 	g_return_if_fail(task->priv != NULL);
 	g_free(task->package);
+	g_object_unref(task->priv->network);
 	G_OBJECT_CLASS(pk_task_parent_class)->finalize(object);
 }
 
