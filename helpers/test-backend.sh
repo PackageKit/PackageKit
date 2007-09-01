164174a9b59f394a6226405fa7af4d9c7b2165cd
Richard Hughes <richard@hughsie.com>
2007-09-01 01:27:46 +0100

    fix test script

--- helpers/test-backend.sh
+++ helpers/test-backend.sh
@@ -6,58 +6,58 @@ package_id="BasiliskII;1.0-0.20060501.1.fc7;x86_64;freshrpms"
 backend="yum"
 
 echo "get deps $package_id"
-./$backend-get-deps.py $package_id 
+./$backend/get-deps.py $package_id 
 echo "exitcode=$?"
 
 echo "get description $package_id"
-./$backend-get-description.py $package_id
+./$backend/get-description.py $package_id
 echo "exitcode=$?"
 
 echo "get updates"
-./$backend-get-updates.py
+./$backend/get-updates.py
 echo "exitcode=$?"
 
 echo "refresh cache"
-./$backend-refresh-cache.py
+./$backend/refresh-cache.py
 echo "exitcode=$?"
 
 echo "remove $package_id"
-./$backend-remove.py no $package_id
+./$backend/remove.py no $package_id
 echo "exitcode=$?"
 
 echo "remove $package_id (already removed)"
-./$backend-remove.py no $package_id
+./$backend/remove.py no $package_id
 echo "exitcode=$?"
 
 echo "install $package_id"
-./$backend-install.py $package_id
+./$backend/install.py $package_id
 echo "exitcode=$?"
 
 echo "install $package_id (already installed)"
-./$backend-install.py $package_id
+./$backend/install.py $package_id
 echo "exitcode=$?"
 
 echo "search details lm_sensors"
-./$backend-search-details.py none lm_sensors
+./$backend/search-details.py none lm_sensors
 echo "exitcode=$?"
 
 echo "search file gpm-prefs.glade"
-./$backend-search-file.py none gpm-prefs.glade
+./$backend/search-file.py none gpm-prefs.glade
 echo "exitcode=$?"
 
 echo "search group system"
-./$backend-search-group.py none system
+./$backend/search-group.py none system
 echo "exitcode=$?"
 
 echo "search name power"
-./$backend-search-name.py none power
+./$backend/search-name.py none power
 echo "exitcode=$?"
 
 echo "update $package_id"
-./$backend-update.py $package_id
+./$backend/update.py $package_id
 echo "exitcode=$?"
 
 echo "update system"
-./$backend-update-system.py
+./$backend/update-system.py
 echo "exitcode=$?"
 
