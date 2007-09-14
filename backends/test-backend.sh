aeb32491cc40ebf862eabe77fb9c14d5671ac147
Elliot Peele <elliot@localhost.localdomain>
2007-09-14 00:05:18 -0400

    clean up paths

--- backends/test-backend.sh
+++ backends/test-backend.sh
@@ -6,58 +6,57 @@ package_id="BasiliskII;1.0-0.20060501.1.fc7;x86_64;freshrpms"
 backend="yum"
 
 echo "get deps $package_id"
-./$backend/get-deps.py $package_id 
+./$backend/helpers/get-deps.py $package_id 
 echo "exitcode=$?"
 
 echo "get description $package_id"
-./$backend/get-description.py $package_id
+./$backend/helpers/get-description.py $package_id
 echo "exitcode=$?"
 
 echo "get updates"
-./$backend/get-updates.py
+./$backend/helpers/get-updates.py
 echo "exitcode=$?"
 
 echo "refresh cache"
-./$backend/refresh-cache.py
+./$backend/helpers/refresh-cache.py
 echo "exitcode=$?"
 
 echo "remove $package_id"
-./$backend/remove.py no $package_id
+./$backend/helpers/remove.py no $package_id
 echo "exitcode=$?"
 
 echo "remove $package_id (already removed)"
-./$backend/remove.py no $package_id
+./$backend/helpers/remove.py no $package_id
 echo "exitcode=$?"
 
 echo "install $package_id"
-./$backend/install.py $package_id
+./$backend/helpers/install.py $package_id
 echo "exitcode=$?"
 
 echo "install $package_id (already installed)"
-./$backend/install.py $package_id
+./$backend/helpers/install.py $package_id
 echo "exitcode=$?"
 
 echo "search details lm_sensors"
-./$backend/search-details.py none lm_sensors
+./$backend/helpers/search-details.py none lm_sensors
 echo "exitcode=$?"
 
 echo "search file gpm-prefs.glade"
-./$backend/search-file.py none gpm-prefs.glade
+./$backend/helpers/search-file.py none gpm-prefs.glade
 echo "exitcode=$?"
 
 echo "search group system"
-./$backend/search-group.py none system
+./$backend/helpers/search-group.py none system
 echo "exitcode=$?"
 
 echo "search name power"
-./$backend/search-name.py none power
+./$backend/helpers/search-name.py none power
 echo "exitcode=$?"
 
 echo "update $package_id"
-./$backend/update.py $package_id
+./$backend/helpers/update.py $package_id
 echo "exitcode=$?"
 
 echo "update system"
-./$backend/update-system.py
+./$backend/helpers/update-system.py
 echo "exitcode=$?"
-
