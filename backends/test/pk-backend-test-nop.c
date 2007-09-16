b3054fd6ebce583e4559e45e7acb91d916225f19
Richard Hughes <richard@hughsie.com>
2007-09-16 20:52:59 +0100

    add different types of test backends

---
+++ backends/test/pk-backend-test-nop.c
@@ -0,0 +1,50 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2007 Richard Hughes <richard@hughsie.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <gmodule.h>
+#include <glib.h>
+#include <pk-backend.h>
+
+PK_BACKEND_OPTIONS (
+	"Test NOP Backend",			/* description */
+	"0.0.1",				/* version */
+	"Richard Hughes <richard@hughsie.com>",	/* author */
+	NULL,					/* initalize */
+	NULL,					/* destroy */
+	NULL,					/* get_groups */
+	NULL,					/* get_filters */
+	NULL,					/* cancel_job_try */
+	NULL,					/* get_depends */
+	NULL,					/* get_description */
+	NULL,					/* get_requires */
+	NULL,					/* get_update_detail */
+	NULL,					/* get_updates */
+	NULL,					/* install_package */
+	NULL,					/* refresh_cache */
+	NULL,					/* remove_package */
+	NULL,					/* search_details */
+	NULL,					/* search_file */
+	NULL,					/* search_group */
+	NULL,					/* search_name */
+	NULL,					/* update_package */
+	NULL					/* update_system */
+);
+
--- backends/test/pk-backend-test-thread.c
+++ backends/test/pk-backend-test-thread.c
@@ -221,7 +221,7 @@ backend_update_system (PkBackend *backend)
 }
 
 PK_BACKEND_OPTIONS (
-	"Test Backend",				/* description */
+	"Test Thread Backend",			/* description */
 	"0.0.1",				/* version */
 	"Richard Hughes <richard@hughsie.com>",	/* author */
 	backend_initalize,			/* initalize */
