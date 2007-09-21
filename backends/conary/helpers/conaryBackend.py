247549eac189e320f0c0f756fc14baf694b2af01
Ken VanDine <ken@vandine.org>
2007-09-21 14:35:47 -0400

    * Fixed read access to db * Actually commit the transaction when syncing the cache * Handle TypeErr when removing duplicate search results

--- backends/conary/helpers/conaryBackend.py
+++ backends/conary/helpers/conaryBackend.py
@@ -57,8 +57,12 @@ class PackageKitConaryBackend(PackageKitBaseBackend):
 
         # Remove dupes
         tempDict = {}
-        for element in troveTupleList:
-            tempDict[element] = None
+        try:
+            for element in troveTupleList:
+                tempDict[element] = None
+        except TypeError:
+            del tempDict  # move on to the next method
+        else:
             troveTupleList = tempDict.keys()
 
         # Get the latest first
@@ -333,7 +337,7 @@ class Cache(object):
 
         self.conn = dbstore.connect(os.path.join(self.dbPath, self.dbName))
         self.cursor = self.conn.cursor()
-        self.cursor.execute("PRAGMA count_changes=0")
+        self.cursor.execute("PRAGMA count_changes=0", start_transaction=False)
 
         if os.path.isfile(os.path.join(self.dbPath, self.dbName)):
             self._validate_tables()
@@ -466,6 +470,7 @@ class Cache(object):
 
         try:
             self.cursor.execute(sql, values)
+            self.conn.commit()
         except Exception,e:
             print str(e)
 
