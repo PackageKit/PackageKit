ddfa95a7122f0ed30304cdaeee0d9aeb48ec5e6b
Robin Norwood <rnorwood@redhat.com>
2007-10-16 11:29:34 -0400

    Fix License and copyright notices.

--- python/pk-frontend-test.py
+++ python/pk-frontend-test.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python
 #
+# Licensed under the GNU General Public License Version 2
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+# Copyright (C) 2007 Tom Parker <palfrey@tevp.net>
+
 # "pkt" python test program for PackageKit
 #
 # pkt serves both as a simple PackageKit client, and as an example user of the
 # PackageKit python API
-#
-# Copyright (C) 2007 Tom Parker <palfrey@tevp.net>
-#
-# Licensed under the GNU General Public License Version 2
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License v2 as published by
-# the Free Software Foundation.
 
 from sys import argv,exit
 from optparse import OptionParser
