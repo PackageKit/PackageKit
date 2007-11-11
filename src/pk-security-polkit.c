d81126f026fe3dc757a9afca600b906571c52338
Richard Hughes <richard@hughsie.com>
2007-11-11 22:44:39 +0000

    fix make check

--- src/pk-security-polkit.c
+++ src/pk-security-polkit.c
@@ -281,7 +281,7 @@ libst_security (LibSelfTest *test)
 	/************************************************************/
 	libst_title (test, "map valid role to action");
 	action = pk_security_role_to_action (security, PK_ROLE_ENUM_UPDATE_PACKAGE);
-	if (pk_strequal (action, "org.freedesktop.packagekit.update") == TRUE) {
+	if (pk_strequal (action, "org.freedesktop.packagekit.update-package") == TRUE) {
 		libst_success (test, NULL, error);
 	} else {
 		libst_failed (test, "did not get correct action '%s'", action);
