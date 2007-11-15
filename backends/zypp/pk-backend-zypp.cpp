2510369980876c630486d015d51154dc978d952a
Boyd Timothy <btimothy@gmail.com>
2007-11-15 08:23:17 -0700

    Cleaned up zypp makefile to use ZYPP_CFLAGS/LIBS from configure.ac

--- backends/zypp/pk-backend-zypp.cpp
+++ backends/zypp/pk-backend-zypp.cpp
@@ -24,56 +24,18 @@
 #include <string.h>
 #include <pk-backend.h>
 
-#include <zypp/ZYppFactory.h>
-#include <zypp/ResObject.h>
-#include <zypp/ResPoolProxy.h>
-#include <zypp/ui/Selectable.h>
-#include <zypp/Patch.h>
-#include <zypp/Selection.h>
-#include <zypp/Package.h>
-#include <zypp/Pattern.h>
-#include <zypp/Language.h>
-#include <zypp/Product.h>
-#include <zypp/Repository.h>
+//#include <zypp/ZYppFactory.h>
+//#include <zypp/ResObject.h>
+//#include <zypp/ResPoolProxy.h>
+//#include <zypp/ui/Selectable.h>
+//#include <zypp/Patch.h>
+//#include <zypp/Selection.h>
+//#include <zypp/Package.h>
+//#include <zypp/Pattern.h>
+//#include <zypp/Language.h>
+//#include <zypp/Product.h>
+//#include <zypp/Repository.h>
 #include <zypp/RepoManager.h>
-
-/**
- * backend_get_groups:
- */
-/*
-static void
-backend_get_groups (PkBackend *backend, PkEnumList *elist)
-{
-	g_return_if_fail (backend != NULL);
-	pk_enum_list_append_multiple (elist,
-				      PK_GROUP_ENUM_ACCESSORIES,
-				      PK_GROUP_ENUM_GAMES,
-				      PK_GROUP_ENUM_GRAPHICS,
-				      PK_GROUP_ENUM_INTERNET,
-				      PK_GROUP_ENUM_OFFICE,
-				      PK_GROUP_ENUM_OTHER,
-				      PK_GROUP_ENUM_PROGRAMMING,
-				      PK_GROUP_ENUM_MULTIMEDIA,
-				      PK_GROUP_ENUM_SYSTEM,
-				      -1);
-}
-*/
-
-/**
- * backend_get_filters:
- */
-/*
-static void
-backend_get_filters (PkBackend *backend, PkEnumList *elist)
-{
-	g_return_if_fail (backend != NULL);
-	pk_enum_list_append_multiple (elist,
-				      PK_FILTER_ENUM_GUI,
-				      PK_FILTER_ENUM_INSTALLED,
-				      PK_FILTER_ENUM_DEVELOPMENT,
-				      -1);
-}
-*/
 
 /**
  * backend_get_repo_list:
@@ -100,28 +62,28 @@ extern "C" PK_BACKEND_OPTIONS (
 	"Boyd Timothy <btimothy@gmail.com>, Scott Reeves <sreeves@novell.com>",	/* author */
 	NULL,					/* initalize */
 	NULL,					/* destroy */
-	NULL,			/* get_groups */
-	NULL,			/* get_filters */
+	NULL,					/* get_groups */
+	NULL,					/* get_filters */
 	NULL,					/* cancel */
 	NULL,					/* get_depends */
-	NULL,		/* get_description */
+	NULL,					/* get_description */
 	NULL,					/* get_files */
 	NULL,					/* get_requires */
 	NULL,					/* get_update_detail */
-	NULL,			/* get_updates */
-	NULL,		/* install_package */
+	NULL,					/* get_updates */
+	NULL,					/* install_package */
 	NULL,					/* install_file */
-	NULL,			/* refresh_cache */
-	NULL,			/* remove_package */
+	NULL,					/* refresh_cache */
+	NULL,					/* remove_package */
 	NULL,					/* resolve */
 	NULL,					/* rollback */
-	NULL,			/* search_details */
+	NULL,					/* search_details */
 	NULL,					/* search_file */
-	NULL,			/* search_group */
-	NULL,			/* search_name */
-	NULL,			/* update_package */
-	NULL,			/* update_system */
-	backend_get_repo_list,					/* get_repo_list */
+	NULL,					/* search_group */
+	NULL,					/* search_name */
+	NULL,					/* update_package */
+	NULL,					/* update_system */
+	backend_get_repo_list,			/* get_repo_list */
 	NULL,					/* repo_enable */
 	NULL					/* repo_set_data */
 );
