75f39e5711bb6dd2df603ef265a8584e89cb394f
Richard Hughes <richard@hughsie.com>
2008-01-24 18:41:35 +0000

    make the apt backend compile

--- backends/apt/pk-backend-apt.c
+++ backends/apt/pk-backend-apt.c
@@ -23,10 +23,44 @@
 #include <gmodule.h>
 #include <glib.h>
 #include <string.h>
+#include <pk-network.h>
 #include <pk-backend.h>
-#include <pk-backend-python.h>
+#include <pk-backend-spawn.h>
 #include "pk-apt-search.h"
 #include "config.h"
+
+static PkBackendSpawn *spawn;
+static PkNetwork *network;
+
+/**
+ * backend_initalize:
+ * This should only be run once per backend load, i.e. not every transaction
+ */
+static void
+backend_initalize (PkBackend *backend)
+{
+	g_return_if_fail (backend != NULL);
+	pk_debug ("FILTER: initalize");
+	network = pk_network_new ();
+	spawn = pk_backend_spawn_new ();
+	pk_backend_spawn_set_name (spawn, "yum");
+	backend_init_search (backend);
+}
+
+/**
+ * backend_destroy:
+ * This should only be run once per backend load, i.e. not every transaction
+ */
+static void
+backend_destroy (PkBackend *backend)
+{
+	g_return_if_fail (backend != NULL);
+	g_return_if_fail (spawn != NULL);
+	pk_debug ("FILTER: destroy");
+	backend_finish_search (backend);
+	g_object_unref (network);
+	g_object_unref (spawn);
+}
 
 /**
  * backend_get_groups:
@@ -62,33 +96,168 @@ backend_get_filters (PkBackend *backend, PkEnumList *elist)
 				      -1);
 }
 
+/**
+ * pk_backend_bool_to_text:
+ */
+static const gchar *
+pk_backend_bool_to_text (gboolean value)
+{
+	if (value == TRUE) {
+		return "yes";
+	}
+	return "no";
+}
+
+/**
+ * backend_get_depends:
+ */
+static void
+backend_get_depends (PkBackend *backend, const gchar *package_id, gboolean recursive)
+{
+	g_return_if_fail (backend != NULL);
+	g_return_if_fail (spawn != NULL);
+	pk_backend_spawn_helper (spawn, "get-depends.py", package_id, pk_backend_bool_to_text (recursive), NULL);
+}
+
+/**
+ * backend_get_updates:
+ */
+static void
+backend_get_updates (PkBackend *backend)
+{
+	g_return_if_fail (backend != NULL);
+	g_return_if_fail (spawn != NULL);
+	pk_backend_spawn_helper (spawn, "get-updates.py", NULL);
+}
+
+/**
+ * backend_install_package:
+ */
+static void
+backend_install_package (PkBackend *backend, const gchar *package_id)
+{
+	g_return_if_fail (backend != NULL);
+	g_return_if_fail (spawn != NULL);
+
+	/* check network state */
+	if (pk_network_is_online (network) == FALSE) {
+		pk_backend_error_code (backend, PK_ERROR_ENUM_NO_NETWORK, "Cannot install when offline");
+		pk_backend_finished (backend);
+		return;
+	}
+
+	pk_backend_spawn_helper (spawn, "install.py", package_id, NULL);
+}
+
+/**
+ * backend_refresh_cache:
+ */
+static void
+backend_refresh_cache (PkBackend *backend, gboolean force)
+{
+	g_return_if_fail (backend != NULL);
+	g_return_if_fail (spawn != NULL);
+
+	/* check network state */
+	if (pk_network_is_online (network) == FALSE) {
+		pk_backend_error_code (backend, PK_ERROR_ENUM_NO_NETWORK, "Cannot refresh cache whilst offline");
+		pk_backend_finished (backend);
+		return;
+	}
+
+	pk_backend_spawn_helper (spawn, "refresh-cache.py", NULL);
+}
+
+/**
+ * pk_backend_remove_package:
+ */
+static void
+backend_remove_package (PkBackend *backend, const gchar *package_id, gboolean allow_deps)
+{
+	g_return_if_fail (backend != NULL);
+	g_return_if_fail (spawn != NULL);
+	pk_backend_spawn_helper (spawn, "remove.py", pk_backend_bool_to_text (allow_deps), package_id, NULL);
+}
+
+/**
+ * pk_backend_update_package:
+ */
+static void
+backend_update_package (PkBackend *backend, const gchar *package_id)
+{
+	g_return_if_fail (backend != NULL);
+	g_return_if_fail (spawn != NULL);
+
+	/* check network state */
+	if (pk_network_is_online (network) == FALSE) {
+		pk_backend_error_code (backend, PK_ERROR_ENUM_NO_NETWORK, "Cannot install when offline");
+		pk_backend_finished (backend);
+		return;
+	}
+
+	pk_backend_spawn_helper (spawn, "update.py", package_id, NULL);
+}
+
+/**
+ * pk_backend_update_system:
+ */
+static void
+backend_update_system (PkBackend *backend)
+{
+	g_return_if_fail (backend != NULL);
+	g_return_if_fail (spawn != NULL);
+	pk_backend_spawn_helper (spawn, "update-system.py", NULL);
+}
+
+/**
+ * pk_backend_resolve:
+ */
+static void
+backend_resolve (PkBackend *backend, const gchar *filter, const gchar *package_id)
+{
+	g_return_if_fail (backend != NULL);
+	g_return_if_fail (spawn != NULL);
+	pk_backend_spawn_helper (spawn, "resolve.py", filter, package_id, NULL);
+}
+
+/**
+ * pk_backend_get_repo_list:
+ */
+static void
+backend_get_repo_list (PkBackend *backend)
+{
+	g_return_if_fail (backend != NULL);
+	g_return_if_fail (spawn != NULL);
+	pk_backend_spawn_helper (spawn, "get-repo-list.py", NULL);
+}
+
 PK_BACKEND_OPTIONS (
 	"Apt (with " APT_SEARCH " searching)",				/* description */
 	"Ali Sabil <ali.sabil@gmail.com>; Tom Parker <palfrey@tevp.net>",	/* author */
-	backend_init_search,					/* initalize */
-	backend_finish_search,					/* destroy */
+	backend_initalize,			/* initalize */
+	backend_destroy,			/* destroy */
 	backend_get_groups,			/* get_groups */
 	backend_get_filters,			/* get_filters */
 	NULL,					/* cancel */
-	pk_backend_python_get_depends,					/* get_depends */
+	backend_get_depends,			/* get_depends */
 	backend_get_description,		/* get_description */
 	NULL,					/* get_files */
 	NULL,					/* get_requires */
 	NULL,					/* get_update_detail */
-	pk_backend_python_get_updates,			/* get_updates */
-	pk_backend_python_install_package,		/* install_package */
+	backend_get_updates,			/* get_updates */
+	backend_install_package,		/* install_package */
 	NULL,					/* install_file */
-	pk_backend_python_refresh_cache,			/* refresh_cache */
-	pk_backend_python_remove_package,			/* remove_package */
-	pk_backend_python_resolve,					/* resolve */
+	backend_refresh_cache,			/* refresh_cache */
+	backend_remove_package,			/* remove_package */
+	backend_resolve,			/* resolve */
 	NULL,					/* rollback */
 	backend_search_details,			/* search_details */
 	NULL,					/* search_file */
 	backend_search_group,			/* search_group */
 	backend_search_name,			/* search_name */
-	pk_backend_python_update_package,			/* update_package */
-	pk_backend_python_update_system,			/* update_system */
-	pk_backend_python_get_repo_list,					/* get_repo_list */
+	backend_update_package,			/* update_package */
+	backend_update_system,			/* update_system */
+	backend_get_repo_list,			/* get_repo_list */
 	NULL,					/* repo_enable */
 	NULL					/* repo_set_data */
 );
