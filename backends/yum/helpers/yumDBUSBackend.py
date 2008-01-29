9f5f01c5d065b9b95c6ee2a8adaf25844d6eb1d5
Richard Hughes <richard@hughsie.com>
2008-01-29 19:53:58 +0000

    correct the dbus service name

--- backends/yum/helpers/yumDBUSBackend.py
+++ backends/yum/helpers/yumDBUSBackend.py
@@ -223,7 +223,7 @@ class PackageKitYumBackend(PackageKitBaseBackend):
 
     def __init__(self,lock=True):
         signal.signal(signal.SIGQUIT, sigquit)
-        PackageKitBaseBackend.__init__(self,backend = 'yum')
+        PackageKitBaseBackend.__init__(self,backend = 'Yum')
         self.yumbase = PackageKitYumBase()
         yumbase = self.yumbase
         self._setup_yum()
