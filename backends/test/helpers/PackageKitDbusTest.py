960447bdfc48f7fd7fe30e3948965cf12b9f2233
Richard Hughes <richard@hughsie.com>
2008-02-07 22:59:02 +0000

    add the other sigals to the dbus test backend

--- backends/test/helpers/PackageKitDbusTest.py
+++ backends/test/helpers/PackageKitDbusTest.py
@@ -55,6 +55,76 @@ class PackageKitTestBackendService(dbus.service.Object):
     def Finished(self, exit):
         print "Finished (%d)" % (exit)
 
+    @dbus.service.signal(dbus_interface=PACKAGEKIT_DBUS_INTERFACE,
+                         signature='ssb')
+    def RepoDetail(self, repo_id, description, enabled):
+        print "RepoDetail (%s, %s, %i)" % (repo_id, description, enabled)
+
+    @dbus.service.signal(dbus_interface=PACKAGEKIT_DBUS_INTERFACE,
+                         signature='u')
+    def StatusChanged(self, status):
+        print "StatusChanged (%i)" % (status)
+
+    @dbus.service.signal(dbus_interface=PACKAGEKIT_DBUS_INTERFACE,
+                         signature='u')
+    def PercentageChanged(self, percentage):
+        print "PercentageChanged (%i)" % (percentage)
+
+    @dbus.service.signal(dbus_interface=PACKAGEKIT_DBUS_INTERFACE,
+                         signature='u')
+    def SubPercentageChanged(self, percentage):
+        print "SubPercentageChanged (%i)" % (percentage)
+
+    @dbus.service.signal(dbus_interface=PACKAGEKIT_DBUS_INTERFACE,
+                         signature='')
+    def NoPercentageChanged(self):
+        print "NoPercentageChanged"
+
+    @dbus.service.signal(dbus_interface=PACKAGEKIT_DBUS_INTERFACE,
+                         signature='uss')
+    def Package(self, percentage, package_id, summary):
+        print "Package (%s, %s)" % (package_id, summary)
+
+    @dbus.service.signal(dbus_interface=PACKAGEKIT_DBUS_INTERFACE,
+                         signature='ssussu')
+    def Description(self, package_id, licence, group, detail, url, size):
+        print "Description (%s, %s, %u, %s, %s, %u)" % (package_id, licence, group, detail, url, size)
+
+    @dbus.service.signal(dbus_interface=PACKAGEKIT_DBUS_INTERFACE,
+                         signature='ssussu')
+    def Files(self, package_id, file_list):
+        print "Files (%s, %s)" % (package_id, file_list)
+
+    @dbus.service.signal(dbus_interface=PACKAGEKIT_DBUS_INTERFACE,
+                         signature='sssssssu')
+    def UpdateDetail(self, package_id, updates, obsoletes, vendor_url, bugzilla_url, cve_url, restart, update_text):
+        print "UpdateDetail (%s, %s, %s, %s, %s, %s, %s, %u)" % (package_id, updates, obsoletes, vendor_url, bugzilla_url, cve_url, restart, update_text)
+
+    @dbus.service.signal(dbus_interface=PACKAGEKIT_DBUS_INTERFACE,
+                         signature='b')
+    def AllowCancel(self, allow_cancel):
+        print "AllowCancel (%i)" % (allow_cancel)
+
+    @dbus.service.signal(dbus_interface=PACKAGEKIT_DBUS_INTERFACE,
+                         signature='us')
+    def ErrorCode(self, code, description):
+        print "ErrorCode (%i, %s)" % (code, description)
+
+    @dbus.service.signal(dbus_interface=PACKAGEKIT_DBUS_INTERFACE,
+                         signature='us')
+    def RequireRestart(self, restart, description):
+        print "RequireRestart (%i, %s)" % (restart, description)
+
+    @dbus.service.signal(dbus_interface=PACKAGEKIT_DBUS_INTERFACE,
+                         signature='us')
+    def Message(self, message, description):
+        print "Message (%i, %s)" % (message, description)
+
+    @dbus.service.signal(dbus_interface=PACKAGEKIT_DBUS_INTERFACE,
+                         signature='ssssssu')
+    def RepoSignatureRequired(self, repository_name, key_url, key_userid, key_id, key_fingerprint, key_timestamp, sig_type):
+        print "RepoSignatureRequired (%s, %s, %s, %s, %s, %s, %i)" % (repository_name, key_url, key_userid, key_id, key_fingerprint, key_timestamp, sig_type)
+
 bus = dbus.SystemBus()
 bus_name = dbus.service.BusName(PACKAGEKIT_DBUS_SERVICE, bus=bus)
 manager = PackageKitTestBackendService(bus_name, PACKAGEKIT_DBUS_PATH)
