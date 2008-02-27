61c59e15a5d4161ef1f4d3a43ff914ca190c70e0
Richard Hughes <richard@hughsie.com>
2008-02-27 01:05:34 +0100

    test the spawned backend stderr and stdout functionality better

--- backends/test/helpers/search-name.sh
+++ backends/test/helpers/search-name.sh
@@ -9,11 +9,17 @@
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 
-echo "no-percentage-updates" > /dev/stderr
-echo "status	query" > /dev/stderr
+echo -e "no-percentage-updates" > /dev/stderr
 sleep 1
-echo "package	available	glib2;2.14.0;i386;fedora	The GLib library"
+echo -e "percentage\t10" > /dev/stderr
+echo -e "status	query" > /dev/stderr
 sleep 1
-echo "package	installed	gtk2;gtk2-2.11.6-6.fc8;i386;fedora	GTK+ Libraries for GIMP"
+echo -e "percentage\t30" > /dev/stderr
+echo -e "package	available	glib2;2.14.0;i386;fedora	The GLib library"
+sleep 1
+echo -e "percentage\t70" > /dev/stderr
+echo -e "package	installed	gtk2;gtk2-2.11.6-6.fc8;i386;fedora	GTK+ Libraries for GIMP"
+sleep 1
+echo -e "percentage\t100" > /dev/stderr
 exit 0
 
