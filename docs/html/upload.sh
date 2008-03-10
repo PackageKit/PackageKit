f663b5b432ed2bad338be679efbcde7eab4e0b88
Richard Hughes <richard@hughsie.com>
2008-03-10 23:44:39 +0000

    update all the gtk-doc pages

--- docs/html/upload.sh
+++ docs/html/upload.sh
@@ -7,7 +7,7 @@ scp *.html $USER@$SERVER:/$LOCATION/
 scp *.css $USER@$SERVER:/$LOCATION/
 scp ../spec/pk-reference.html $USER@$SERVER:/$LOCATION/
 scp ../spec/pk-*.png $USER@$SERVER:/$LOCATION/
-scp ../api/html/PackageKit-pk-client.html $USER@$SERVER:/$LOCATION/gtk-doc/
+scp ../api/html/*.html $USER@$SERVER:/$LOCATION/gtk-doc/
 scp ../api/html/style.css $USER@$SERVER:/$LOCATION/gtk-doc/
 
 
