c8864b48f42716a000c5f18a63f4c6f1d51bf3ff
Richard Hughes <richard@hughsie.com>
2008-03-11 21:15:48 +0000

    fix the uppload script

--- docs/html/upload.sh
+++ docs/html/upload.sh
@@ -8,6 +8,6 @@ scp *.css $USER@$SERVER:/$LOCATION/
 scp ../spec/pk-reference.html $USER@$SERVER:/$LOCATION/
 scp ../spec/pk-*.png $USER@$SERVER:/$LOCATION/
 scp ../api/html/*.html $USER@$SERVER:/$LOCATION/gtk-doc/
-scp ../api/html/style.css $USER@$SERVER:/$LOCATION/gtk-doc/
-
+scp ../api/html/*.png $USER@$SERVER:/$LOCATION/gtk-doc/
+scp ../api/html/*.css $USER@$SERVER:/$LOCATION/gtk-doc/
 
