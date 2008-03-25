6ae22b8d0bf5861eddee4b8132407b0567f6953b
Robin Norwood <rnorwood@redhat.com>
2008-03-25 10:34:04 -0400

    Add removed enums back.

--- libpackagekit/pk-enum.c
+++ libpackagekit/pk-enum.c
@@ -120,6 +120,8 @@ static PkEnumMatch enum_error[] = {
 	{PK_ERROR_ENUM_REPO_NOT_FOUND,		"repo-not-found"},
 	{PK_ERROR_ENUM_CANNOT_REMOVE_SYSTEM_PACKAGE,	"cannot-remove-system-package"},
 	{PK_ERROR_ENUM_PROCESS_KILL,		"process-kill"},
+        {PK_ERROR_ENUM_FAILED_INITIALIZATION,   "failed-initialization"},
+        {PK_ERROR_ENUM_FAILED_FINALISE,         "failed-finalise"},
 	{PK_ERROR_ENUM_FAILED_CONFIG_PARSING,	"failed-config-parsing"},
 	{PK_ERROR_ENUM_CANNOT_CANCEL,		"cannot-cancel"},
 	{PK_ERROR_ENUM_CANNOT_GET_LOCK,         "cannot-get-lock"},
