6518a3cd8c06a989cfc94a089cf557cf87065731
Stefan Haas <shaas@suse.de>
2008-04-07 12:21:58 +0200

    add the install_signature backend method, and reorder the methods

--- backends/zypp/pk-backend-zypp.cpp
+++ backends/zypp/pk-backend-zypp.cpp
@@ -1764,30 +1764,31 @@ extern "C" PK_BACKEND_OPTIONS (
 	"Boyd Timothy <btimothy@gmail.com>, Scott Reeves <sreeves@novell.com>, Stefan Haas <shaas@suse.de>",	/* author */
 	backend_initialize,			/* initalize */
 	backend_destroy,			/* destroy */
-	backend_get_groups,			/* get_groups */
 	backend_get_filters,			/* get_filters */
+	backend_get_groups,			/* get_groups */
 	NULL,					/* cancel */
 	backend_get_depends,			/* get_depends */
 	backend_get_description,		/* get_description */
 	backend_get_files,			/* get_files */
+	backend_get_repo_list,			/* get_repo_list */
 	backend_get_requires,			/* get_requires */
 	backend_get_update_detail,		/* get_update_detail */
 	backend_get_updates,			/* get_updates */
+	NULL,					/* install signature */
 	backend_install_package,		/* install_package */
 	NULL,					/* install_file */
 	backend_refresh_cache,			/* refresh_cache */
 	backend_remove_package,			/* remove_package */
+	backend_repo_enable,			/* repo_enable */
+	backend_repo_set_data,			/* repo_set_data */
 	backend_resolve,			/* resolve */
 	NULL,					/* rollback */
 	backend_search_details,			/* search_details */
 	backend_search_file,			/* search_file */
 	backend_search_group,    		/* search_group */
 	backend_search_name,			/* search_name */
+	NULL,                                   /* service_pack */
 	backend_update_packages,                /* update_packages */
 	backend_update_system,			/* update_system */
-	backend_get_repo_list,			/* get_repo_list */
-	backend_repo_enable,			/* repo_enable */
-	backend_repo_set_data,			/* repo_set_data */
-	NULL,                                   /* service_pack */
 	backend_what_provides                   /* what_provides */
 );
