8041515ab8bdc27d99f0aaeada4088faf291ac2d
Richard Hughes <richard@hughsie.com>
2008-04-08 13:39:35 +0100

    we only support auth on the the transaction interface

--- libpackagekit/pk-polkit-client.c
+++ libpackagekit/pk-polkit-client.c
@@ -54,6 +54,9 @@ static void     pk_polkit_client_finalize		(GObject           *object);
 #define POLKIT_DBUS_PATH		"/org/gnome/PolicyKit/Manager"
 #define POLKIT_DBUS_INTERFACE		"org.gnome.PolicyKit.Manager"
 
+/* we only support auth on the the transaction interface */
+#define	PK_ERROR_REFUSED_BY_POLICY	"org.freedesktop.PackageKit.Transaction.RefusedByPolicy"
+
 /**
  * PkPolkitClientPrivate:
  *
@@ -172,7 +175,7 @@ pk_polkit_client_error_denied_by_policy (GError *error)
 	/* check for specific error */
 	error_name = dbus_g_error_get_name (error);
 	pk_debug ("ERROR: %s: %s", error_name, error->message);
-	if (pk_strequal (error_name, "org.freedesktop.PackageKit.RefusedByPolicy")) {
+	if (pk_strequal (error_name, PK_ERROR_REFUSED_BY_POLICY)) {
 		return TRUE;
 	}
 	return FALSE;
