ebed0613d9c5b325970dba6eb74dbbc078005103
Richard Hughes <richard@hughsie.com>
2008-04-09 22:23:23 +0100

    make pkcon be cleverer when doing the moving bars - hopefully it's more sane now

--- client/pk-console.c
+++ client/pk-console.c
@@ -39,17 +39,17 @@
 #include <pk-common.h>
 #include <pk-connection.h>
 
-#define PROGRESS_BAR_PADDING 22
-#define MINIMUM_COLUMNS (PROGRESS_BAR_PADDING + 5)
+#define PROGRESS_BAR_SIZE 15
 
 static GMainLoop *loop = NULL;
 static PkEnumList *role_list = NULL;
 static gboolean is_console = FALSE;
-static gboolean has_output = FALSE;
-static gboolean printed_bar = FALSE;
+static gboolean has_output_bar = FALSE;
 static gboolean need_requeue = FALSE;
 static gboolean nowait = FALSE;
+static gboolean awaiting_space = FALSE;
 static guint timer_id = 0;
+static guint percentage_last = 0;
 static PkControl *control = NULL;
 static PkClient *client = NULL;
 static PkClient *client_task = NULL;
@@ -61,23 +61,52 @@ typedef struct {
 } PulseState;
 
 /**
+ * pk_console_bar:
+ **/
+static void
+pk_console_bar (guint subpercentage)
+{
+	guint section;
+	guint i;
+
+	/* don't pretty print */
+	if (!is_console) {
+		return;
+	}
+	if (!has_output_bar) {
+		return;
+	}
+	/* restore cursor */
+	g_print ("%c8", 0x1B);
+
+	section = (guint) ((gfloat) PROGRESS_BAR_SIZE / (gfloat) 100.0 * (gfloat) subpercentage);
+	g_print ("[");
+	for (i=0; i<section; i++) {
+		g_print ("=");
+	}
+	for (i=0; i<PROGRESS_BAR_SIZE-section; i++) {
+		g_print (" ");
+	}
+	g_print ("] ");
+	if (percentage_last != PK_CLIENT_PERCENTAGE_INVALID) {
+		g_print ("(%i%%)", percentage_last);
+	} else {
+		g_print ("       ");
+	}
+	awaiting_space = TRUE;
+}
+
+/**
  * pk_console_package_cb:
  **/
 static void
 pk_console_package_cb (PkClient *client, PkInfoEnum info, const gchar *package_id, const gchar *summary, gpointer data)
 {
 	PkPackageId *ident;
-	PkPackageId *spacing;
-	gchar *info_text;
-	guint extra = 0;
-
-	/* if on console, clear the progress bar line */
-	if (is_console && printed_bar && has_output == FALSE) {
-		g_print ("\r");
-	}
-
-	/* pass this out */
-	info_text = pk_strpad (pk_info_enum_to_text (info), 12);
+	PkRoleEnum role;
+	gchar *package = NULL;
+	gchar *info_pad = NULL;
+	gchar *package_pad = NULL;
 
 	/* split */
 	ident = pk_package_id_new_from_string (package_id);
@@ -86,25 +115,60 @@ pk_console_package_cb (PkClient *client, PkInfoEnum info, const gchar *package_i
 		return;
 	}
 
-	/* these numbers are guesses */
-	extra = 0;
-	spacing = pk_package_id_new ();
-	spacing->name = pk_strpad_extra (ident->name, 20, &extra);
-	spacing->arch = pk_strpad_extra (ident->arch, 7, &extra);
-	spacing->version = pk_strpad_extra (ident->version, 15, &extra);
-	spacing->data = pk_strpad_extra (ident->data, 12, &extra);
+	/* make these all the same lenght */
+	info_pad = pk_strpad (pk_info_enum_to_text (info), 12);
+
+	/* don't pretty print */
+	if (!is_console) {
+		g_print ("%s %s-%s.%s\n", info_pad, ident->name, ident->version, ident->arch);
+		goto out;
+	}
+
+	/* pad the name-version */
+	if (pk_strzero (ident->version)) {
+		package = g_strdup (ident->name);
+	} else {
+		package = g_strdup_printf ("%s-%s", ident->name, ident->version);
+	}
+	package_pad = pk_strpad (package, 40);
+
+	/* mark previous complete */
+	if (has_output_bar) {
+		pk_console_bar (100);
+	}
+
+	if (awaiting_space) {
+		g_print ("\n");
+	}
+
+	pk_client_get_role (client, &role, NULL, NULL);
+	if (role == PK_ROLE_ENUM_SEARCH_NAME ||
+	    role == PK_ROLE_ENUM_SEARCH_GROUP ||
+	    role == PK_ROLE_ENUM_SEARCH_FILE ||
+	    role == PK_ROLE_ENUM_SEARCH_DETAILS ||
+	    role == PK_ROLE_ENUM_GET_DEPENDS ||
+	    role == PK_ROLE_ENUM_GET_REQUIRES ||
+	    role == PK_ROLE_ENUM_GET_UPDATES) {
+		/* don't do the bar */
+		g_print ("%s %s\n", info_pad, package_pad);
+		goto out;
+	}
+
+	has_output_bar = TRUE;
+	/* do we need to new line? */
 
 	/* pretty print */
-	g_print ("%s %s %s %s %s %s\n", info_text, spacing->name,
-		 spacing->arch, spacing->version, spacing->data, summary);
+	g_print ("%s %s ", info_pad, package_pad);
 
+	/* save cursor in new position */
+	g_print ("%c7", 0x1B);
+	pk_console_bar (0);
+out:
 	/* free all the data */
-	g_free (info_text);
 	pk_package_id_free (ident);
-	pk_package_id_free (spacing);
-
-	/* don't do the percentage bar from now on */
-	has_output = TRUE;
+	g_free (package);
+	g_free (info_pad);
+	g_free (package_pad);
 }
 
 /**
@@ -117,6 +181,9 @@ pk_console_transaction_cb (PkClient *client, const gchar *tid, const gchar *time
 {
 	const gchar *role_text;
 	role_text = pk_role_enum_to_text (role);
+	if (awaiting_space) {
+		g_print ("\n");
+	}
 	g_print ("Transaction  : %s\n", tid);
 	g_print (" timespec    : %s\n", timespec);
 	g_print (" succeeded   : %i\n", succeeded);
@@ -135,6 +202,9 @@ pk_console_update_detail_cb (PkClient *client, const gchar *package_id,
 			     const gchar *cve_url, PkRestartEnum restart,
 			     const gchar *update_text, gpointer data)
 {
+	if (awaiting_space) {
+		g_print ("\n");
+	}
 	g_print (_("Update detail\n"));
 	g_print ("  package:    '%s'\n", package_id);
 	if (pk_strzero (updates) == FALSE) {
@@ -169,6 +239,9 @@ pk_console_repo_detail_cb (PkClient *client, const gchar *repo_id,
 {
 	gchar *repo;
 	repo = pk_strpad (repo_id, 28);
+	if (awaiting_space) {
+		g_print ("\n");
+	}
 	if (enabled) {
 		g_print ("  enabled   %s %s\n", repo, description);
 	} else {
@@ -178,124 +251,54 @@ pk_console_repo_detail_cb (PkClient *client, const gchar *repo_id,
 }
 
 /**
- * pk_console_get_terminal_columns:
- **/
-static guint
-pk_console_get_terminal_columns (void)
-{
-	struct winsize ws;
-
-	ioctl (1, TIOCGWINSZ, &ws);
-	if (ws.ws_col < MINIMUM_COLUMNS) {
-		return MINIMUM_COLUMNS;
-	}
-
-	return ws.ws_col;
-}
-
-/**
- * pk_console_draw_progress_bar:
- **/
-static void
-pk_console_draw_progress_bar (guint percentage, guint remaining_time)
-{
-	guint i;
-	guint progress_bar_size = pk_console_get_terminal_columns () - PROGRESS_BAR_PADDING;
-	guint progress = (gint) (progress_bar_size * (gfloat) (percentage) / 100);
-	guint remaining = progress_bar_size - progress;
-
-	/* have we already been spinning? */
-	if (timer_id != 0) {
-		g_source_remove (timer_id);
-		timer_id = 0;
-	}
-
-	/* we need to do an extra line */
-	printed_bar = TRUE;
-
-	g_print ("\r    [");
-	for (i = 0; i < progress; i++) {
-		g_print ("=");
-	}
-	for (i = 0; i < remaining; i++) {
-		g_print (".");
-	}
-	g_print ("]  %3i%%", percentage);
-	if (remaining_time != 0) {
-		if (remaining_time > 60) {
-			guint remaining_minutes = remaining_time / 60;
-			if (remaining_minutes > 60) {
-				guint remaining_hours = remaining_time / 3600;
-				g_print (" (%2ih eta)", remaining_hours);
-			} else {
-				g_print (" (%2im eta)", remaining_minutes);
-			}
-		} else {
-			g_print (" (%2is eta)", remaining_time);
-		}
-	} else {
-		g_print ("          ");
-	}
-	if (percentage == 100) {
-		g_print ("\n");
-	}
-}
-
-/**
  * pk_console_pulse_bar:
  **/
 static gboolean
 pk_console_pulse_bar (PulseState *pulse_state)
 {
 	guint i;
-	guint progress_bar_size = pk_console_get_terminal_columns () - PROGRESS_BAR_PADDING;
-	gchar *padding;
 
-	/* don't spin if we have had output */
-	if (has_output) {
-		return FALSE;
+	if (!has_output_bar) {
+		return TRUE;
 	}
 
-	/* we need to do an extra line */
-	printed_bar = TRUE;
-
-	/* the clever pulse code */
-	printf("\r    [");
-	for (i = 0; i < pulse_state->position - 1; i++) {
-		g_print (".");
-	}
-	printf("===");
-	for (i = pulse_state->position; i < progress_bar_size - 2; i++) {
-		g_print (".");
-	}
-	g_print ("]");
+	/* restore cursor */
+	g_print ("%c8", 0x1B);
 
 	if (pulse_state->move_forward) {
-		if (pulse_state->position == progress_bar_size - 2) {
+		if (pulse_state->position == PROGRESS_BAR_SIZE - 1) {
 			pulse_state->move_forward = FALSE;
-			pulse_state->position--;
 		} else {
 			pulse_state->position++;
 		}
 	} else if (pulse_state->move_forward == FALSE) {
 		if (pulse_state->position == 1) {
 			pulse_state->move_forward = TRUE;
-			pulse_state->position++;
 		} else {
 			pulse_state->position--;
 		}
 	}
 
-	/* Move the cursor off the screen. */
-	padding = g_strnfill (PROGRESS_BAR_PADDING - 6, ' ');
-	g_print ("%s", padding);
-	g_free (padding);
+	g_print ("[");
+	for (i=0; i<pulse_state->position-1; i++) {
+		g_print (" ");
+	}
+	printf("==");
+	for (i=0; i<PROGRESS_BAR_SIZE-pulse_state->position-1; i++) {
+		g_print (" ");
+	}
+	g_print ("] ");
+	if (percentage_last != PK_CLIENT_PERCENTAGE_INVALID) {
+		g_print ("(%i%%)", percentage_last);
+	} else {
+		g_print ("        ");
+	}
 
 	return TRUE;
 }
 
 /**
- * pk_console_draw_progress_bar:
+ * pk_console_draw_pulse_bar:
  **/
 static void
 pk_console_draw_pulse_bar (void)
@@ -306,7 +309,6 @@ pk_console_draw_pulse_bar (void)
 	if (timer_id != 0) {
 		return;
 	}
-	has_output = FALSE;
 	if (is_console) {
 		pulse_state.position = 1;
 		pulse_state.move_forward = TRUE;
@@ -321,14 +323,24 @@ static void
 pk_console_progress_changed_cb (PkClient *client, guint percentage, guint subpercentage,
 				guint elapsed, guint remaining, gpointer data)
 {
-	if (is_console) {
-		if (percentage == PK_CLIENT_PERCENTAGE_INVALID) {
-			pk_console_draw_pulse_bar ();
+	if (!is_console) {
+		if (percentage != PK_CLIENT_PERCENTAGE_INVALID) {
+			g_print ("percentage: %i%%\n", percentage);
 		} else {
-			pk_console_draw_progress_bar (percentage, remaining);
+			g_print ("percentage: unknown\n");
 		}
+		return;
+	}
+	percentage_last = percentage;
+	if (subpercentage == PK_CLIENT_PERCENTAGE_INVALID) {
+		pk_console_bar (0);
+		pk_console_draw_pulse_bar ();
 	} else {
-		g_print ("%i%%\n", percentage);
+		if (timer_id != 0) {
+			g_source_remove (timer_id);
+			timer_id = 0;
+		}
+		pk_console_bar (subpercentage);
 	}
 }
 
@@ -337,22 +349,21 @@ static const gchar *summary =
 	"\n"
 	"Subcommands:\n"
 	"  search name|details|group|file data\n"
-	"  install <package_id>\n"
+	"  install <package>\n"
 	"  install-file <file>\n"
 	"  install-signature <type> <key_id> <package_id>\n"
-	"  remove <package_id>\n"
-	"  update <package_id>\n"
+	"  remove <package>\n"
+	"  update <package>\n"
 	"  refresh\n"
 	"  resolve\n"
-	"  force-refresh\n"
 	"  update-system\n"
 	"  get updates\n"
-	"  get depends <package_id>\n"
-	"  get requires <package_id>\n"
-	"  what-provides <search>\n"
-	"  get description <package_id>\n"
-	"  get files <package_id>\n"
-	"  get updatedetail <package_id>\n"
+	"  get depends <package>\n"
+	"  get requires <package>\n"
+	"  provides <search>\n"
+	"  get description <package>\n"
+	"  get files <package>\n"
+	"  get updatedetail <package>\n"
 	"  get actions\n"
 	"  get groups\n"
 	"  get filters\n"
@@ -360,9 +371,7 @@ static const gchar *summary =
 	"  get repos\n"
 	"  enable-repo <repo_id>\n"
 	"  disable-repo <repo_id>\n"
-	"  set-repo-data <repo_id> <parameter> <value>\n"
-	"\n"
-	"  package_id is typically gimp;2:2.4.0-0.rc1.1.fc8;i386;development";
+	"  set-repo-data <repo_id> <parameter> <value>\n";
 
 /**
  * pk_console_signature_finished_cb:
@@ -389,29 +398,29 @@ static void
 pk_console_finished_cb (PkClient *client, PkExitEnum exit, guint runtime, gpointer data)
 {
 	PkRoleEnum role;
-	gchar *blanking;
 	const gchar *role_text;
 	gfloat time;
 	PkRestartEnum restart;
 
 	pk_client_get_role (client, &role, NULL, NULL);
 
+	/* mark previous complete */
+	if (has_output_bar) {
+		pk_console_bar (100);
+	}
+
 	/* cancel the spinning */
 	if (timer_id != 0) {
 		g_source_remove (timer_id);
 	}
 
-	/* if on console, clear the progress bar line */
-	if (is_console && printed_bar && has_output == FALSE) {
-		g_print ("\r");
-		blanking = g_strnfill (pk_console_get_terminal_columns (), ' ');
-		g_print ("%s", blanking);
-		g_free (blanking);
-		g_print ("\r");
-	}
-
 	role_text = pk_role_enum_to_text (role);
 	time = (gfloat) runtime / 1000.0;
+
+	/* do we need to new line? */
+	if (awaiting_space) {
+		g_print ("\n");
+	}
 	g_print ("%s runtime was %.1f seconds\n", role_text, time);
 
 	/* is there any restart to notify the user? */
@@ -512,6 +521,9 @@ pk_console_perhaps_resolve (PkClient *client, PkFilterEnum filter, const gchar *
 	}
 
 	/* else list the options if multiple matches found */
+	if (awaiting_space) {
+		g_print ("\n");
+	}
 	g_print (_("There are multiple matches\n"));
 	for (i=0; i<length; i++) {
 		item = pk_client_package_buffer_get_item (client_task, i);
@@ -556,7 +568,6 @@ pk_console_remove_only (PkClient *client, const gchar *package_id, gboolean forc
 	if (!ret) {
 		return ret;
 	}
-	pk_client_set_synchronous (client, TRUE, NULL);
 	return pk_client_remove_package (client, package_id, force, autoremove, error);
 }
 
@@ -655,6 +666,9 @@ pk_console_remove_package (PkClient *client, const gchar *package, GError **erro
 	}
 
 	/* present this to the user */
+	if (awaiting_space) {
+		g_print ("\n");
+	}
 	g_print (_("The following packages have to be removed:\n"));
 	for (i=0; i<length; i++) {
 		item = pk_client_package_buffer_get_item (client_task, i);
@@ -878,8 +892,8 @@ pk_console_process_commands (PkClient *client, int argc, char *argv[], GError **
 		}
 	} else if (strcmp (mode, "update") == 0) {
 		if (value == NULL) {
-			g_set_error (error, 0, 0, _("specify a package to update"));
-			return FALSE;
+			/* do the system update */
+			ret = pk_client_update_system (client, error);
 		} else {
 			ret = pk_console_update_package (client, value, error);
 		}
@@ -956,7 +970,7 @@ pk_console_process_commands (PkClient *client, int argc, char *argv[], GError **
 			} else {
 				ret = pk_console_get_requires (client, details, error);
 			}
-		} else if (strcmp (value, "what-provides") == 0) {
+		} else if (strcmp (value, "provides") == 0) {
 			if (details == NULL) {
 				g_set_error (error, 0, 0, _("specify a search term"));
 				return FALSE;
@@ -1001,12 +1015,8 @@ pk_console_process_commands (PkClient *client, int argc, char *argv[], GError **
 		} else {
 			g_set_error (error, 0, 0, _("invalid get type"));
 		}
-	} else if (strcmp (mode, "update-system") == 0) {
-		ret = pk_client_update_system (client, error);
 	} else if (strcmp (mode, "refresh") == 0) {
 		ret = pk_client_refresh_cache (client, FALSE, error);
-	} else if (strcmp (mode, "force-refresh") == 0) {
-		ret = pk_client_refresh_cache (client, TRUE, error);
 	} else {
 		g_set_error (error, 0, 0, _("option not yet supported"));
 	}
@@ -1030,9 +1040,7 @@ pk_console_error_code_cb (PkClient *client, PkErrorCodeEnum error_code, const gc
 		pk_debug ("ignoring GPG error as handled");
 		return;
 	}
-
-	/* if on console, clear the progress bar line */
-	if (is_console && printed_bar) {
+	if (awaiting_space) {
 		g_print ("\n");
 	}
 	g_print ("Error: %s : %s\n", pk_error_enum_to_text (error_code), details);
@@ -1048,7 +1056,7 @@ pk_console_description_cb (PkClient *client, const gchar *package_id,
 			   gulong size, gpointer data)
 {
 	/* if on console, clear the progress bar line */
-	if (is_console && printed_bar) {
+	if (awaiting_space) {
 		g_print ("\n");
 	}
 	g_print (_("Package description\n"));
@@ -1069,11 +1077,9 @@ pk_console_files_cb (PkClient *client, const gchar *package_id,
 {
 	gchar **filevector = g_strsplit (filelist, ";", 0);
 
-	/* if on console, clear the progress bar line */
-	if (is_console && printed_bar) {
+	if (awaiting_space) {
 		g_print ("\n");
 	}
-
 	g_print ("Package files\n");
 
 	if (*filevector != NULL) {
@@ -1103,6 +1109,9 @@ pk_console_repo_signature_required_cb (PkClient *client, const gchar *package_id
 	gboolean ret;
 	GError *error = NULL;
 
+	if (awaiting_space) {
+		g_print ("\n");
+	}
 	g_print ("Repository Signature Required\n");
 	g_print ("Package:     %s\n", package_id);
 	g_print ("Name:        %s\n", repository_name);
@@ -1141,6 +1150,9 @@ static void
 pk_connection_changed_cb (PkConnection *pconnection, gboolean connected, gpointer data)
 {
 	/* if the daemon crashed, don't hang around */
+	if (awaiting_space) {
+		g_print ("\n");
+	}
 	if (connected == FALSE) {
 		g_print (_("The daemon crashed mid transaction. This is bad\n"));
 		exit (2);
