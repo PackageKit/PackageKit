a7bdaa5f7fac52e444e27b405d3640140d1050e0
Richard Hughes <richard@hughsie.com>
2008-06-20 16:21:28 +0100

    move the spec files into the autogenerated gtk-doc directory

--- docs/html/upload.sh
+++ docs/html/upload.sh
@@ -14,8 +14,6 @@ LOCATION="/srv/www/html"
 scp img/*.png $USER@$SERVER:/$LOCATION/img/
 scp *.html $USER@$SERVER:/$LOCATION/
 scp *.css $USER@$SERVER:/$LOCATION/
-scp ../spec/pk-reference.html $USER@$SERVER:/$LOCATION/
-scp ../spec/pk-*.png $USER@$SERVER:/$LOCATION/
 scp ../api/html/*.html $USER@$SERVER:/$LOCATION/gtk-doc/
 scp ../api/html/*.png $USER@$SERVER:/$LOCATION/gtk-doc/
 scp ../api/html/*.css $USER@$SERVER:/$LOCATION/gtk-doc/
