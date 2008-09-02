d4f09f99632a428619c8e17a96d9848c31fc2c1e
Richard Hughes <richard@hughsie.com>
2008-09-02 12:08:36 +0100

    bugfix: fix the formatting in the enum list, as COULD_NOT_FIND_PACKAGE was not being picked up by the enum converter

--- libpackagekit/pk-enum.c
+++ libpackagekit/pk-enum.c
@@ -184,7 +184,7 @@ static PkEnumMatch enum_message[] = {
 	{PK_MESSAGE_ENUM_CACHE_BEING_REBUILT,	"cache-being-rebuilt"},
 	{PK_MESSAGE_ENUM_UNTRUSTED_PACKAGE,	"untrusted-package"},
 	{PK_MESSAGE_ENUM_NEWER_PACKAGE_EXISTS,	"newer-package-exists"},
-	{PK_MESSAGE_ENUM_COULD_NOT_FIND_PACKAGE,"could-not-find-package"},
+	{PK_MESSAGE_ENUM_COULD_NOT_FIND_PACKAGE,	"could-not-find-package"},
 	{PK_MESSAGE_ENUM_CONFIG_FILES_CHANGED,	"config-files-changed"},
 	{PK_MESSAGE_ENUM_PACKAGE_ALREADY_INSTALLED, "package-already-installed"},
 	{0, NULL}
