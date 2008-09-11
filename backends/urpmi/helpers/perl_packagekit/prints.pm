6136d25b85425ccb1faaa4a91758718e1737d52b
Aurelien Lefebvre <alefebvre@mandriva.com>
2008-09-11 12:44:35 +0200

    urpmi: Added licence headers

--- backends/urpmi/helpers/perl_packagekit/prints.pm
+++ backends/urpmi/helpers/perl_packagekit/prints.pm
@@ -1,3 +1,14 @@
+#
+# Copyright (C) 2008 Aurelien Lefebvre <alefebvre@mandriva.com>
+#
+# Licensed under the GNU General Public License Version 2
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+
 package perl_packagekit::prints;
 
 use Exporter;
