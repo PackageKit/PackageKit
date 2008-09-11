ec7c09f09af634e6ed09950ebda52822fac715bd
Aurelien Lefebvre <alefebvre@mandriva.com>
2008-09-11 12:44:35 +0200

    urpmi: Added licence headers

--- backends/urpmi/helpers/get-requires.pl
+++ backends/urpmi/helpers/get-requires.pl
@@ -1,4 +1,14 @@
 #!/usr/bin/perl
+#
+# Copyright (C) 2008 Aurelien Lefebvre <alefebvre@mandriva.com>
+#
+# Licensed under the GNU General Public License Version 2
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
 
 use strict;
 
