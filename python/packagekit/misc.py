cc6f7c3c34e6ab4e20334c5c7b602876f0d4fb25
Tim Lauridsen <timlau@fedoraproject.org>
2008-09-21 11:53:10 +0200

    python API: return objects instead of dict

---
+++ python/packagekit/misc.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+#
+# (c) 2008
+##    Tim Lauridsen <timlau@fedoraproject.org>
+
+# Misc classes and funtions
+
+def _to_utf8( obj, errors='replace'):
+    '''convert 'unicode' to an encoded utf-8 byte string '''
+    if isinstance(obj, unicode):
+        obj = obj.encode('utf-8', errors)
+    return obj
+
+class PackageKitPackage:
+    ''' 
+    container class from values from the Package signal 
+    '''
+    def __init__(self,installed,id,summary):
+        self.installed = (installed == 'installed')
+        self.id = str(id)
+        self.summary = _to_utf8(summary)
+        
+    def __str__(self):
+        (name,ver,arch,repo) = tuple(self.id.split(";"))
+        p =  "%s-%s.%s" % (name,ver,arch)
+        if self.installed:
+            inst = "installed"
+        else:
+            inst = "available"
+        return "%-40s : %s : %s" % (p, inst, self.summary)
+        
+        
+class PackageKitDistroUpgrade:
+    ''' 
+    container class from values from the DistroUpgrade signal 
+    '''
+    def __init__(self,upgrade_type,name,summary):
+        self.upgrade_type = upgrade_type
+        self.name = name
+        self.summary = _to_utf8(summary)
+        
+    def __str__(self):
+        return " type : %s, name : %s, summary : %s " % (
+                self.upgrade_type,self.name,self.summmary)        
+
+
+class PackageKitDetails:
+    ''' 
+    container class from values from the Detail signal 
+    '''
+    def __init__(self, id, license, group, detail, url, size):
+        self.id = str(id)
+        self.license = license
+        self.group = group
+        self.detail = _to_utf8(detail)
+        self.url = url
+        self.size = size
+        
+class PackageKitUpdateDetails:
+    ''' 
+    container class from values from the UpdateDetail signal 
+    '''
+    def __init__(self, id, updates, obsoletes, vendor_url, bugzilla_url, \
+                 cve_url, restart, update_text, changelog, state, \
+                 issued, updated):
+        self.id = str(id)
+        self.updates = updates
+        self.obsoletes = opsoletes
+        self.vendor_url = vendor_url
+        self.bugzilla_url = bugzilla_url
+        self.cve_url = cve_url
+        self.restart = restart
+        self.update_text = update_text
+        self.changelog = changelog
+        self.state = state
+        self.issued = issued                     
+        self.updated = updated
+        
+class PackageKitRepos:
+    ''' 
+    container class from values from the Repos signal 
+    '''
+    def __init__(self,id, description, enabled):
+        self.id = str(id)
+        self.description = description
+        self.enabled = enabled
+        
+        
+class PackageKitFiles:
+    ''' 
+    container class from values from the Files signal 
+    '''        
+    def __init__(self,id, files):
+        self.id = str(id)
+        self.files = files
+        
+            
+            
