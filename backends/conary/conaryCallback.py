55c8edf834bf42dc29ec9ef7bba704d16c2ca43d
Richard Hughes <richard@hughsie.com>
2008-09-26 14:20:38 +0100

    trivial: convert the conary backend to using the dispatcher, and make pylint a little happier

--- backends/conary/conaryCallback.py
+++ backends/conary/conaryCallback.py
@@ -1,6 +1,4 @@
-#
-# Copyright (c) 2007 Elliot Peele <elliot@bentlogic.net>
-#
+#!/usr/bin/python
 # Licensed under the GNU General Public License Version 2
 #
 # This program is free software; you can redistribute it and/or modify
@@ -8,6 +6,17 @@
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Copyright (C) 2007 Elliot Peele <elliot@bentlogic.net>
+# Copyright (C) 2008 Richard Hughes <richard@hughsie.com>
 
 from conary import callbacks
 from packagekit.backend import *
