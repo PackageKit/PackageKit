1156d344737214d5529e7b4cf80801c40eb3c069
Richard Hughes <richard@hughsie.com>
2008-10-09 15:39:49 +0100

    trivial: add another screenshot

--- docs/html/upload.sh
+++ docs/html/upload.sh
@@ -11,15 +11,14 @@ USER="hughsie"
 SERVER="packagekit.org"
 LOCATION="/srv/www/html"
 
-scp files/*.c $USER@$SERVER:/$LOCATION/files/
 scp img/*.png $USER@$SERVER:/$LOCATION/img/
 scp *.html $USER@$SERVER:/$LOCATION/
 scp *.css $USER@$SERVER:/$LOCATION/
 scp ../api/html/*.html $USER@$SERVER:/$LOCATION/gtk-doc/
 scp ../api/html/*.png $USER@$SERVER:/$LOCATION/gtk-doc/
 scp ../api/html/*.css $USER@$SERVER:/$LOCATION/gtk-doc/
-docbook2pdf ../../man/pkcon.xml --output ./man
-docbook2pdf ../../man/pkmon.xml --output ./man
-docbook2pdf ../../man/pkgenpack.xml --output ./man
-scp man/*.pdf $USER@$SERVER:/$LOCATION/files/
+docbook2pdf ../../man/pkcon.xml --output ./files
+docbook2pdf ../../man/pkmon.xml --output ./files
+docbook2pdf ../../man/pkgenpack.xml --output ./files
+scp files/* $USER@$SERVER:/$LOCATION/files/
 
