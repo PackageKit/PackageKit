7812b6478dd7bcbc6616e7bd3fe6e042e0b8fbc3
Richard Hughes <richard@hughsie.com>
2008-10-17 18:39:00 +0100

    bugfix: if the transaction failed then don't try to get the file lists

--- src/pk-transaction.c
+++ src/pk-transaction.c
@@ -575,8 +575,9 @@ pk_transaction_finished_cb (PkBackend *backend, PkExitEnum exit, PkTransaction *
 	g_signal_handler_disconnect (transaction->priv->backend, transaction->priv->signal_category);
 
 	/* check for session restarts */
-	if (transaction->priv->role == PK_ROLE_ENUM_UPDATE_SYSTEM ||
-	    transaction->priv->role == PK_ROLE_ENUM_UPDATE_PACKAGES) {
+	if (exit == PK_EXIT_ENUM_SUCCESS &&
+	    (transaction->priv->role == PK_ROLE_ENUM_UPDATE_SYSTEM ||
+	     transaction->priv->role == PK_ROLE_ENUM_UPDATE_PACKAGES)) {
 
 		/* check updated packages file lists and running processes */
 		ret = pk_conf_get_bool (transaction->priv->conf, "UpdateCheckProcesses");
