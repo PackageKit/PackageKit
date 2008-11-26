1b8308f50efdc6e96d9387e36e013ec06dfa23a2
Richard Hughes <richard@hughsie.com>
2008-11-26 12:10:07 +0000

    bugfix: don't emit a critical warning if g_desktop_app_info_new_from_filename() returns an error

--- src/pk-post-trans.c
+++ src/pk-post-trans.c
@@ -224,14 +224,18 @@ pk_post_trans_sqlite_add_filename_details (PkPostTrans *post, const gchar *filen
 	gchar *statement;
 	gchar *error_msg = NULL;
 	sqlite3_stmt *sql_statement = NULL;
-	gint rc;
+	gint rc = -1;
 	gint show = TRUE;
 #ifdef PK_BUILD_GIO
-	GAppInfo *info;
+	GDesktopAppInfo *info;
 
 	/* find out if we should show desktop file in menus */
-	info = G_APP_INFO(g_desktop_app_info_new_from_filename (filename));
-	show = g_app_info_should_show (info);
+	info = g_desktop_app_info_new_from_filename (filename);
+	if (info == NULL) {
+		egg_warning ("could not load desktop file %s", filename);
+		goto out;
+	}
+	show = g_app_info_should_show (G_APP_INFO(info));
 	g_object_unref (info);
 #endif
 
