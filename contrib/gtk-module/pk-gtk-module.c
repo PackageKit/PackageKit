cab2fe0d20756cadbdd60f7e283bb18fb59b8e37
Richard Hughes <richard@hughsie.com>
2008-11-27 08:06:42 +0000

    trivial: rename pango-module to gtk-module on request of behdad

--- contrib/gtk-module/pk-gtk-module.c
+++ contrib/gtk-module/pk-gtk-module.c
@@ -20,7 +20,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#define G_LOG_DOMAIN "MissingFontModule"
+#define G_LOG_DOMAIN "PkGtkModule"
 
 #define PANGO_ENABLE_BACKEND
 #include <fontconfig/fontconfig.h>
