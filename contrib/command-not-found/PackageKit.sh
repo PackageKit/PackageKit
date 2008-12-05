21910f8c2677426b430b4b297ef9e6a17aa8f135
Richard Hughes <richard@hughsie.com>
2008-12-05 10:11:30 +0000

    feature: add initial code to add command-not-found functonality to PackageKit

---
+++ contrib/command-not-found/PackageKit.sh
@@ -0,0 +1,12 @@
+# Copyright (C) 2008 Richard Hughes <richard@hughsie.com>
+#
+# Licensed under the GNU General Public License Version 2
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+command_not_found_handler () {
+	/usr/libexec/pk-command-not-found $1 || return 127
+}
+
