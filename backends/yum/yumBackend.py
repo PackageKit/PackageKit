7f377f4aade72b9023aa90642dc7f999838e740b
Richard Hughes <richard@hughsie.com>
2008-12-26 09:48:59 +0000

    yum: remove all tab characters in update descriptions

--- backends/yum/yumBackend.py
+++ backends/yum/yumBackend.py
@@ -2101,6 +2101,8 @@ class PackageKitYumBackend(PackageKitBaseBackend, PackagekitPackage):
         if notice:
             # Update Details
             desc = notice['description']
+            desc = desc.replace("\t", " ")
+
             # Update References (Bugzilla, CVE ...)
             refs = notice['references']
             if refs:
