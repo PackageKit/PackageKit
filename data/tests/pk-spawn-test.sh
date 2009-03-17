e4a5cfb13187b210086f1a8cf17e2d1d4df46891
Richard Hughes <richard@hughsie.com>
2009-03-17 10:35:25 +0000

    trivial: decrease the speed of the SIGKILL spawn test as sometimes it can finish before the cancel timesout

--- data/tests/pk-spawn-test.sh
+++ data/tests/pk-spawn-test.sh
@@ -6,7 +6,7 @@
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 
-time=0.30
+time=0.5
 
 echo -e "percentage\t0"
 echo -e "percentage\t10"
