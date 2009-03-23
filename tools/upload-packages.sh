228b7a14a8b4577a737c84c877bdfa4be6b27c7e
Richard Hughes <richard@hughsie.com>
2009-03-23 16:08:17 +0000

    trivial: new rpm location

--- tools/upload-packages.sh
+++ tools/upload-packages.sh
@@ -4,10 +4,10 @@ echo "Upload!"
 #ssh packagekit.org "rm /srv/www/html/packages/*.rpm"
 rm -Rf /tmp/repo
 mkdir /tmp/repo
-cp /home/hughsie/rpmbuild/REPOS/fedora/10/i386/PackageKit-* /tmp/repo
-cp /home/hughsie/rpmbuild/REPOS/fedora/10/SRPMS/PackageKit-* /tmp/repo
-cp /home/hughsie/rpmbuild/REPOS/fedora/10/i386/gnome-packagekit-* /tmp/repo
-cp /home/hughsie/rpmbuild/REPOS/fedora/10/SRPMS/gnome-packagekit-* /tmp/repo
+cp /home/hughsie/rpmbuild/REPOS/fedora/11/i386/PackageKit-* /tmp/repo
+cp /home/hughsie/rpmbuild/REPOS/fedora/11/SRPMS/PackageKit-* /tmp/repo
+cp /home/hughsie/rpmbuild/REPOS/fedora/11/i386/gnome-packagekit-* /tmp/repo
+cp /home/hughsie/rpmbuild/REPOS/fedora/11/SRPMS/gnome-packagekit-* /tmp/repo
 cd /tmp/repo
 createrepo -d .
 #scp -r * packagekit.org:/srv/www/html/packages/
