15a5cdd38f48fb4b047e32c0f803d83b5492c088
Daniel Nicoletti <dantti85-pk@yahoo.com.br>
2009-08-24 13:51:32 +0100

    Add the InfoReinstalling enum type
    Signed-off-by: Richard Hughes <richard@hughsie.com>

--- lib/packagekit-glib/pk-enum.c
+++ lib/packagekit-glib/pk-enum.c
@@ -322,6 +322,7 @@ static const PkEnumMatch enum_info[] = {
 	{PK_INFO_ENUM_COLLECTION_INSTALLED,	"collection-installed"},
 	{PK_INFO_ENUM_COLLECTION_AVAILABLE,	"collection-available"},
 	{PK_INFO_ENUM_FINISHED,			"finished"},
+	{PK_INFO_ENUM_REINSTALLING,		"reinstalling"},
 	{0, NULL}
 };
 
