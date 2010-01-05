d29307a5005cd45c0fa21473a02cf457ba10e0d2
Richard Hughes <richard@hughsie.com>
2010-01-05 09:50:46 +0000

    yum: include PackageKit in the list of essential packages

--- backends/yum/yumBackend.py
+++ backends/yum/yumBackend.py
@@ -2162,7 +2162,7 @@ class PackageKitYumBackend(PackageKitBaseBackend, PackagekitPackage):
             else:
                 for txmbr in self.yumbase.tsInfo:
                     pkg = txmbr.po
-                    system_packages = ['yum','rpm', 'glibc']
+                    system_packages = ['yum','rpm','glibc','PackageKit']
                     if pkg.name in system_packages:
                         self.error(ERROR_CANNOT_REMOVE_SYSTEM_PACKAGE, "The package %s is essential to correct operation and cannot be removed using this tool." % pkg.name, exit=False)
                         return
