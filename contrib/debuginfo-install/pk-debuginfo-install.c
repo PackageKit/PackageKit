71ae43ff7faf82522c3bebc9fd7008ffcaa3b887
Richard Hughes <richard@hughsie.com>
2010-01-27 16:32:07 +0000

    Update the Free Software Foundation address

--- contrib/debuginfo-install/pk-debuginfo-install.c
+++ contrib/debuginfo-install/pk-debuginfo-install.c
@@ -16,7 +16,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 /* Test with ./pk-debuginfo-install bzip2-libs-1.0.5-5.fc11.i586 glib2-2.20.3-1.fc11.i586 */
