4c8c78f895d88a7c08d919ff9474ae9cfcea6d92
Richard Hughes <richard@hughsie.com>
2010-03-10 13:10:41 +0000

    trivial: make gdb.sh generally more useful

--- src/gdb.sh
+++ src/gdb.sh
@@ -1,4 +1,25 @@
+#!/bin/sh
+# Copyright (C) 2008 Richard Hughes <richard@hughsie.com>
+#
+# Licensed under the GNU General Public License Version 2
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+if [ "$USER" != "root" ]; then
+    echo "You are not running this script as root. Use sudo."
+    exit 1
+fi
+
+if [ "$1x" = "x" ]; then
+    echo "NO BACKEND SPECIFIED, using dummy"
+    BACKEND=dummy
+else
+    BACKEND=$1
+fi
+
 export G_DEBUG=fatal_criticals
 sudo touch /etc/PackageKit/PackageKit.conf
-sudo G_DEBUG=fatal_criticals gdb --args .libs/lt-packagekitd --verbose --backend=dummy --disable-timer
+sudo G_DEBUG=fatal_criticals gdb --args .libs/lt-packagekitd --verbose --backend=$BACKEND --disable-timer
 
