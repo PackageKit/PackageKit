622d8c040a825ab531d0c8eaa3f301514ec9deab
Daniel Nicoletti <dantti85-pk@yahoo.com.br>
2010-08-25 17:04:30 -0300

    aptcc: Disable auto remove by default, it was said that most users hate this...

--- backends/aptcc/apt.cpp
+++ backends/aptcc/apt.cpp
@@ -1401,7 +1401,7 @@ void aptcc::updateInterface(int fd, int writeFd)
 /* Remove unused automatic packages */
 bool aptcc::DoAutomaticRemove(pkgCacheFile &Cache)
 {
-	bool doAutoRemove = _config->FindB("APT::Get::AutomaticRemove", true);
+	bool doAutoRemove = _config->FindB("APT::Get::AutomaticRemove", false);
 	pkgDepCache::ActionGroup group(*Cache);
 
 	if (_config->FindB("APT::Get::Remove",true) == false &&
