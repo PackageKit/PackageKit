368de5752e84571604c98a823f7f75c27b270570
Per Øyvind Karlsen <peroyvind@mandriva.org>
2011-05-19 23:08:41 +0200

    fix previous commit..

--- backends/urpmi/helpers/urpmi_backend/groups.pm
+++ backends/urpmi/helpers/urpmi_backend/groups.pm
@@ -121,8 +121,8 @@ use constant MDV_GROUPS => {
 sub get_mdv_groups {
   my ($pk_group) = @_;
   my @groups = ();
-  foreach(keys (MDV_GROUPS)) {
-    if(MDV_GROUPS)->{$_} eq $pk_group) {
+  foreach(keys %{(MDV_GROUPS)}) {
+    if((MDV_GROUPS)->{$_} eq $pk_group) {
       push @groups, $_;
     }
   }
