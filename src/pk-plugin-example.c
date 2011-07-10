d82244ffbd4166fa73f135927e88a09619bef84a
Richard Hughes <richard@hughsie.com>
2011-07-10 10:02:51 +0100

    trivial: flesh out the example plugin a little more

--- src/pk-plugin-example.c
+++ src/pk-plugin-example.c
@@ -24,7 +24,7 @@
 #include <pk-transaction.h>
 
 typedef struct {
-	guint		 dummy;
+	guint			 dummy;
 } PluginPrivate;
 
 static PluginPrivate *priv;
@@ -67,7 +67,33 @@ pk_transaction_plugin_destroy (PkTransaction *transaction)
 void
 pk_transaction_plugin_run (PkTransaction *transaction)
 {
+	gboolean ret;
+	PkBackend *backend = NULL;
+	PkConf *conf;
+	PkRoleEnum role;
+
+	/* check the config file */
+	conf = pk_transaction_priv_get_conf (transaction);
+	ret = pk_conf_get_bool (conf, "UseDummy");
+	if (!ret)
+		goto out;
+
+	/* check the role */
+	role = pk_transaction_priv_get_role (transaction);
+	if (role != PK_ROLE_ENUM_REFRESH_CACHE)
+		goto out;
+
+	/* check we can do the action */
+	backend = pk_transaction_priv_get_backend (transaction);
+	if (!pk_backend_is_implemented (backend,
+	    PK_ROLE_ENUM_GET_PACKAGES)) {
+		g_debug ("cannot get packages");
+		goto out;
+	}
+
 	g_debug ("plugin: run");
+out:
+	return;
 }
 
 /**
