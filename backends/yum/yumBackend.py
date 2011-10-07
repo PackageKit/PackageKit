d4e19de93f6e77cf0a3d4714e653899f240b9034
Nils Philippsen <nils@redhat.com>
2011-10-07 17:04:00 +0200

    yum: add ourselves to YumBase.run_with_package_names (cherry picked from commit de285e72bc7ed2be2c17a3c42906e1a56cad8255)

--- backends/yum/yumBackend.py
+++ backends/yum/yumBackend.py
@@ -3454,6 +3454,10 @@ class PackageKitYumBase(yum.YumBase):
     def __init__(self, backend):
         yum.YumBase.__init__(self)
 
+        # YumBase.run_with_package_names is used to record which packages are
+        # involved in executing a transaction
+        self.run_with_package_names.add('PackageKit-yum')
+
         # load the config file
         config = ConfigParser.ConfigParser()
         try:
