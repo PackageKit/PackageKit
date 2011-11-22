32f030b58ac3c7ba97ccc92515687fdd539d98ff
Nils Philippsen <nils@redhat.com>
2011-11-22 15:33:27 +0100

    automatically rebuild moc files with different moc version

--- moc.mk
+++ moc.mk
@@ -1,8 +1,18 @@
-%.moc : %.h
+%.moc : %.h $(top_srcdir)/.moc.version
 	$(MOC) -i -o "$@" "$<"
+
+$(top_srcdir)/.moc.version : $(MOC)
+	# "moc -v" always fails :-/
+	$(MOC) -v > $@.tmp 2>&1 || :
+	if test ! -f "$@" || test "`cat $@`" != "`cat $@.tmp`"; then \
+		mv -f $@.tmp $@; \
+	else \
+		rm -f $@.tmp; \
+	fi
 
 clean-moc-extra:
 	rm -vf *.moc
+	rm -vf $(top_srcdir)/.moc.version
 
 clean-am: clean-moc-extra
 
