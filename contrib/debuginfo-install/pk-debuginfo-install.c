a7085aa011ce07641c39cfe514f44ffed0c2acc3
Per Øyvind Karlsen <peroyvind@mandriva.org>
2011-12-03 02:21:57 +0100

    remove deprecated g_thread_init() on GLib < 2.31

--- contrib/debuginfo-install/pk-debuginfo-install.c
+++ contrib/debuginfo-install/pk-debuginfo-install.c
@@ -531,8 +531,10 @@ main (int argc, char *argv[])
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 	textdomain (GETTEXT_PACKAGE);
 
+#if (GLIB_MAJOR_VERSION == 2 && GLIB_MINOR_VERSION < 31)
 	if (! g_thread_supported ())
 		g_thread_init (NULL);
+#endif
 	g_type_init ();
 
 	context = g_option_context_new (NULL);
