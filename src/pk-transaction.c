a54817ea9c085c3bea77a77ba6f201f849216fd0
Matthias Klumpp <matthias@tenstral.net>
2012-08-01 12:53:11 +0200

    Always make some transaction roles exclusive
    INSTALL_FILES
    INSTALL_PACKAGES
    REMOVE_PACKAGES
    UPDATE_PACKAGES
    UPGRADE_SYSTEM
    REPAIR_SYSTEM
    ...are now considered exclusive by default. This will
    improve performance for these exclusive transactions.

--- src/pk-transaction.c
+++ src/pk-transaction.c
@@ -3084,6 +3084,17 @@ static void
 pk_transaction_set_role (PkTransaction *transaction, PkRoleEnum role)
 {
 	transaction->priv->role = role;
+
+	/* always set transaction exclusive for some actions (improves performance) */
+	if (role == PK_ROLE_ENUM_INSTALL_FILES ||
+	    role == PK_ROLE_ENUM_INSTALL_PACKAGES ||
+	    role == PK_ROLE_ENUM_REMOVE_PACKAGES ||
+	    role == PK_ROLE_ENUM_UPDATE_PACKAGES ||
+	    role == PK_ROLE_ENUM_UPGRADE_SYSTEM ||
+	    role == PK_ROLE_ENUM_REPAIR_SYSTEM) {
+		pk_transaction_make_exclusive (transaction);
+	}
+
 	pk_transaction_emit_property_changed (transaction,
 					      "Role",
 					      g_variant_new_uint32 (role));
