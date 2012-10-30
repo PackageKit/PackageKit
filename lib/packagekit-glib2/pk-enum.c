fc9b6653a3f6ca4c1b27045292f7e09bdfb865ca
Richard Hughes <richard@hughsie.com>
2012-10-30 16:09:17 +0000

    Perform the simulation of spawned transactions correctly
    Backends that were checking for 'simulate' were being sent the typo 'simluate'
    by the daemon which was producing undefined and unexpected results.
    
    Hopefully fixes: https://bugzilla.redhat.com/show_bug.cgi?id=866486

--- lib/packagekit-glib2/pk-enum.c
+++ lib/packagekit-glib2/pk-enum.c
@@ -401,7 +401,7 @@ static const PkEnumMatch enum_upgrade_kind[] = {
 static const PkEnumMatch enum_transaction_flag[] = {
 	{PK_TRANSACTION_FLAG_ENUM_NONE,		"none"},	/* fall though value */
 	{PK_TRANSACTION_FLAG_ENUM_ONLY_TRUSTED,	"only-trusted"},
-	{PK_TRANSACTION_FLAG_ENUM_SIMULATE,	"simluate"},
+	{PK_TRANSACTION_FLAG_ENUM_SIMULATE,	"simulate"},
 	{PK_TRANSACTION_FLAG_ENUM_ONLY_DOWNLOAD, "only-download"},
 	{0, NULL}
 };
