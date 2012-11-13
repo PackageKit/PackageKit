ccdb81d2fc2bcd9474744b93e87955b58b6d8e37
tv <thierry.vignaud@gmail.com>
2012-11-13 10:35:58 +0100

    always use "map" with a block (eg: map { ... } @list)
    (spot by perl_checker)

--- backends/urpmi/helpers/urpmi-dispatched-backend.pl
+++ backends/urpmi/helpers/urpmi-dispatched-backend.pl
@@ -860,7 +860,7 @@ sub _print_package_update_details {
   
   pk_print_update_detail(get_package_id($pkg),
     join("&", @to_upgrade_pkids),
-    join("&", map(fullname_to_package_id($_), @to_remove)),
+    join("&", map { fullname_to_package_id($_) } @to_remove),
     "http://qa.mandriva.com",
     "http://qa.mandriva.com",
     "http://qa.mandriva.com",
