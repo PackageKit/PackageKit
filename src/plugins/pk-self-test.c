996375a88005b4025f3eff95b038f85816df0a25
Anders F Bjorklund <afb@users.sourceforge.net>
2013-05-09 22:22:39 +0200

    trivial: don't miss glib if it's in /usr/local

--- src/plugins/pk-self-test.c
+++ src/plugins/pk-self-test.c
@@ -67,6 +67,7 @@ pk_test_lsof_func (void)
 	pk_test_lsof_get_files_for_directory (files, "/usr/lib64");
 	pk_test_lsof_get_files_for_directory (files, "/usr/lib/x86_64-linux-gnu");
 	pk_test_lsof_get_files_for_directory (files, "/usr/lib/i386-linux-gnu");
+	pk_test_lsof_get_files_for_directory (files, "/usr/local/lib");
 	g_assert_cmpint (files->len, >, 0);
 	g_ptr_array_add (files, NULL);
 	pids = pk_lsof_get_pids_for_filenames (lsof, (gchar**) files->pdata);
