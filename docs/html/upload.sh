f1d052ffa39bb0e3a733c254cf20a32eded6847d
Richard Hughes <richard@hughsie.com>
2013-11-29 09:30:33 +0000

    Update the website and developer docs to reflect reality

--- docs/html/upload.sh
+++ docs/html/upload.sh
@@ -21,7 +21,6 @@ scp ../api/html/*.png $USER@$SERVER:/$LOCATION/gtk-doc/
 scp ../api/html/*.css $USER@$SERVER:/$LOCATION/gtk-doc/
 docbook2pdf ../../man/pkcon.xml --output ./files
 docbook2pdf ../../man/pkmon.xml --output ./files
-docbook2pdf ../../man/pkgenpack.xml --output ./files
 scp files/* $USER@$SERVER:/$LOCATION/files/
 scp ../../contrib/PackageKit.catalog $USER@$SERVER:/$LOCATION/files/
 docbook2html ../../../gnome-packagekit/docs/dbus/org.freedesktop.PackageKit.ref.xml
