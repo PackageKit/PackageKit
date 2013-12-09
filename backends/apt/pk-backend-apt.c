5fe6bb26ec96b673f8aec05ec254240b17418793
Richard Hughes <richard@hughsie.com>
2013-12-09 08:38:45 +0000

    apt: Adapt to the WhatProvides API change

--- backends/apt/pk-backend-apt.c
+++ backends/apt/pk-backend-apt.c
@@ -494,16 +494,14 @@ pk_backend_repo_set_data (PkBackend *backend, PkBackendJob *job, const gchar *ri
  * backend_what_provides:
  */
 void
-pk_backend_what_provides (PkBackend *backend, PkBackendJob *job, PkBitfield filters, PkProvidesEnum provides, gchar **values)
+pk_backend_what_provides (PkBackend *backend, PkBackendJob *job, PkBitfield filters, gchar **values)
 {
 	gchar *search_text;
 	gchar *filters_text;
-	const gchar *provides_text;
 
-	provides_text = pk_provides_enum_to_string (provides);
 	filters_text = pk_filter_bitfield_to_string (filters);
 	search_text = g_strjoinv ("&", values);
-	pk_backend_spawn_helper (spawn, job, BACKEND_FILE, "what-provides", filters_text, provides_text, search_text, NULL);
+	pk_backend_spawn_helper (spawn, job, BACKEND_FILE, "what-provides", filters_text, "any", search_text, NULL);
 	g_free (filters_text);
 	g_free (search_text);
 }
