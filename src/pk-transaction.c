c094c7f9dfb4692a4af811303337133c589dfd0a
Richard Hughes <richard@hughsie.com>
2014-09-03 08:36:40 +0100

    trivial: Remove duplicate transaction state enum string

--- src/pk-transaction.c
+++ src/pk-transaction.c
@@ -799,8 +799,6 @@ pk_transaction_state_to_string (PkTransactionState state)
 	if (state == PK_TRANSACTION_STATE_WAITING_FOR_AUTH)
 		return "waiting-for-auth";
 	if (state == PK_TRANSACTION_STATE_READY)
-		return "committed";
-	if (state == PK_TRANSACTION_STATE_READY)
 		return "ready";
 	if (state == PK_TRANSACTION_STATE_RUNNING)
 		return "running";
