PackageKit/PackageKit

commit 2155b677925357ada0d393e809735edd5a3e0c98

missing part of previous commit..

backends/urpmi/helpers/urpmi_backend/tools.pm
@@ -81,9 +81,8 @@ sub find_installed_fullname {
 }
 
 sub is_package_installed {
-    my ($urpm, $pkg) = @_;
-    my $db = urpm::db_open_or_die_($urpm);
-    return URPM::is_package_installed($db, $pkg);
+    my ($pkg) = @_;
+    return URPM::DB::open()->is_package_installed($pkg);
 }
 
 sub get_package_id {
@@ -141,7 +140,7 @@ sub get_installed_fullname {
   my @depslist = @{$urpm->{depslist}};
   my $pkgname = $pkg->name;
   foreach $_ (@depslist) {
-    if($_->name =~ /^$pkgname$/ && is_package_installed($urpm, $_)) {
+    if($_->name =~ /^$pkgname$/ && is_package_installed($_)) {
       return $_;
     }
   }

backends/urpmi/helpers/urpmi_backend/filters.pm
@@ -55,7 +55,7 @@ sub filter_installed {
   my ($urpm, $pkg, $filter) = @_;
   my $installed;
 
-  $installed = 1 if(is_package_installed($urpm, $pkg));
+  $installed = 1 if(is_package_installed($pkg));
   if($filter eq FILTER_INSTALLED && $installed) {
     return 1;
   }

commit 4ac2a969b23d90f1cd738e81082a418e0a97d0da

urpm: properly use is_package_installed()
bug introduced in commit 2155b677925357ada0d393e809735edd5a3e0c98

backends/urpmi/helpers/urpmi_backend/tools.pm
@@ -87,7 +87,7 @@ sub find_installed_fullname {
 
 sub is_package_installed {
     my ($pkg) = @_;
-    return URPM::DB::open()->is_package_installed($pkg);
+    return URPM::is_package_installed(URPM::DB::open(), $pkg);
 }
 
 sub is_mageia() {