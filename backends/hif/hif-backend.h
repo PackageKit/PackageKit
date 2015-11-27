129de5665dc32c636a4469e45e5bb16dffde2661
Kalev Lember <klember@redhat.com>
2015-11-27 14:48:19 +0100

    trivial: Fix a typo

--- backends/hif/hif-backend.h
+++ backends/hif/hif-backend.h
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
  *
- * Copyright (C) 2014 Richard Hughes <richard@hughsie.com`
+ * Copyright (C) 2014 Richard Hughes <richard@hughsie.com>
  *
  * Licensed under the GNU Lesser General Public License Version 2.1
  *
