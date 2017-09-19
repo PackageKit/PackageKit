61c25100fedf78ad4c372919fe7721872d48b76d
Robert Ancell <robert.ancell@canonical.com>
2017-09-19 18:18:46 +1200

    docs: Don't have clean script remove checked in files

--- docs/api/clean.sh
+++ docs/api/clean.sh
@@ -1,4 +1,9 @@
-rm -f *.txt
+rm -f *-decl.txt
+rm -f *-decl-list.txt
+rm -f *-overrides.txt
+rm -f *-undeclared.txt
+rm -f *-undocumented.txt
+rm -f *-unused.txt
 rm -f PackageKit-scan.*
 rm -f PackageKit.types
 rm -f html/*.html
