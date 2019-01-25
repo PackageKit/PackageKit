f7b5cd495f7fec596fadc595e5ef3750bf5bc210
Corentin Noël <corentin@elementary.io>
2019-01-25 11:56:55 +0100

    Remove Autotools

--- docs/html/thumbnail.sh
+++
@@ -1,9 +0,0 @@
-# rethumbnail the images
-cd img
-images="gpk-*.png kpk-*.png pk-*.png assassin.png list*.png"
-for image in $images; do
-	echo "thumbnailing $image"
-	convert -geometry 300x170 $image thumbnails/$image
-done
-cd -
-
