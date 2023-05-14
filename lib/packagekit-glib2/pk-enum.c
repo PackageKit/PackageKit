PackageKit/PackageKit

commit aa07e0b77bd297cf9fd617ae9b8dd7e0cf218c50

Use the term DDE instead of deepin
Since DDE is the name of the desktop environment, instead of deepin.

lib/packagekit-glib2/pk-enum.c
@@ -263,7 +263,7 @@ static const PkEnumMatch enum_group[] = {
 	{PK_GROUP_ENUM_DESKTOP_GNOME,		"desktop-gnome"},
 	{PK_GROUP_ENUM_DESKTOP_KDE,		"desktop-kde"},
 	{PK_GROUP_ENUM_DESKTOP_XFCE,		"desktop-xfce"},
-	{PK_GROUP_ENUM_DESKTOP_DEEPIN,		"desktop-deepin"},
+	{PK_GROUP_ENUM_DESKTOP_DEEPIN,		"desktop-dde"},
 	{PK_GROUP_ENUM_DESKTOP_OTHER,		"desktop-other"},
 	{PK_GROUP_ENUM_PUBLISHING,		"publishing"},
 	{PK_GROUP_ENUM_SERVERS,			"servers"},

backends/alpm/groups.list
@@ -11,8 +11,8 @@ compiz-fusion-kde	desktop-kde
 compiz-gnome		desktop-gnome
 compiz-gtk		desktop-gnome
 compiz-kde		desktop-kde
-deepin			desktop-deepin
-deepin-extra		desktop-deepin
+deepin			desktop-dde
+deepin-extra		desktop-dde
 e17-extra-svn		desktop-other
 e17-libs-svn		desktop-other
 e17-svn			desktop-other

docs/api/spec/pk-concepts.xml
@@ -696,8 +696,8 @@
             <entry>KDE</entry>
           </row>
           <row>
-            <entry><literal>desktop-deepin</literal></entry>
-            <entry>Deepin (DDE)</entry>
+            <entry><literal>desktop-dde</literal></entry>
+            <entry>DDE</entry>
           </row>
           <row>
             <entry><literal>desktop-other</literal></entry>

lib/packagekit-glib2/pk-enum.h
@@ -523,7 +523,7 @@ typedef enum {
  * @PK_GROUP_ENUM_ACCESSORIES: Accessory packages
  * @PK_GROUP_ENUM_ADMIN_TOOLS: Administration tools packages
  * @PK_GROUP_ENUM_COMMUNICATION: Communication packages
- * @PK_GROUP_ENUM_DESKTOP_DEEPIN: Deepin packages
+ * @PK_GROUP_ENUM_DESKTOP_DDE: DDE packages
  * @PK_GROUP_ENUM_DESKTOP_GNOME: GNOME packages
  * @PK_GROUP_ENUM_DESKTOP_KDE: KDE packages
  * @PK_GROUP_ENUM_DESKTOP_OTHER: Other desktop packages
@@ -564,7 +564,7 @@ typedef enum {
 	PK_GROUP_ENUM_ACCESSORIES,
 	PK_GROUP_ENUM_ADMIN_TOOLS,
 	PK_GROUP_ENUM_COMMUNICATION,
-	PK_GROUP_ENUM_DESKTOP_DEEPIN,
+	PK_GROUP_ENUM_DESKTOP_DDE,
 	PK_GROUP_ENUM_DESKTOP_GNOME,
 	PK_GROUP_ENUM_DESKTOP_KDE,
 	PK_GROUP_ENUM_DESKTOP_OTHER,

commit 69936ca3b9f243b1cb1ea01b54fb77e1f4aec80d

packagekit-glib2: Use the correct enum for DDE
Fixes: aa07e0b77bd297cf9fd617ae9b8dd7e0cf218c50 ("Use the term DDE instead of deepin")

lib/packagekit-glib2/pk-enum.c
@@ -263,7 +263,7 @@ static const PkEnumMatch enum_group[] = {
 	{PK_GROUP_ENUM_DESKTOP_GNOME,		"desktop-gnome"},
 	{PK_GROUP_ENUM_DESKTOP_KDE,		"desktop-kde"},
 	{PK_GROUP_ENUM_DESKTOP_XFCE,		"desktop-xfce"},
-	{PK_GROUP_ENUM_DESKTOP_DEEPIN,		"desktop-dde"},
+	{PK_GROUP_ENUM_DESKTOP_DDE,		"desktop-dde"},
 	{PK_GROUP_ENUM_DESKTOP_OTHER,		"desktop-other"},
 	{PK_GROUP_ENUM_PUBLISHING,		"publishing"},
 	{PK_GROUP_ENUM_SERVERS,			"servers"},