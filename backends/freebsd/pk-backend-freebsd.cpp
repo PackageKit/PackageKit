71aecf3c18305b6afd2b1e56f58a4b2f82275612
Gleb Popov <6yearold@gmail.com>
2023-08-03 18:25:40 +0300

    freebsd backend: Introduce PackageView class.

--- backends/freebsd/pk-backend-freebsd.cpp
+++ backends/freebsd/pk-backend-freebsd.cpp
@@ -44,6 +44,55 @@
 #include <memory>
 #include <vector>
 
+class PackageView
+{
+public:
+    PackageView(struct pkg* pkg)
+    : pk_id(nullptr) {
+        name_el = pkg_get_element(pkg, PKG_NAME);
+        version_el = pkg_get_element(pkg, PKG_VERSION);
+        arch_el = pkg_get_element(pkg, (pkg_attr)XXX_PKG_ARCH); // TODO: Use pkg_asprintf() to get this
+        reponame_el = pkg_get_element(pkg, PKG_REPONAME);
+        comment_el = pkg_get_element(pkg, PKG_COMMENT);
+    }
+
+    ~PackageView() {
+        g_free(pk_id);
+    }
+
+    const gchar* name() const {
+        return name_el->string;
+    }
+
+    const gchar* version() const {
+        return version_el->string;
+    }
+
+    const gchar* comment() const {
+        return comment_el->string;
+    }
+
+    const gchar* arch() const {
+        return arch_el->string;
+    }
+
+    const gchar* repository() const {
+        return reponame_el->string;
+    }
+
+    gchar* packageKitId() {
+        if (!pk_id)
+            pk_id = pk_package_id_build(name_el->string, version_el->string,
+                                        arch_el->string, reponame_el->string);
+        return pk_id;
+    }
+private:
+    struct pkg_el* name_el, *version_el,
+        // TODO: arch or abi?
+        *arch_el, *reponame_el, *comment_el;
+    gchar* pk_id;
+};
+
 typedef struct {
     gboolean has_signature;
     gboolean repo_enabled_devel;
