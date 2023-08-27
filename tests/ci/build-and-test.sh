9c0870c77626f7fe62a25318a3c0e6970fd783bc
Matthias Klumpp <matthias@tenstral.net>
2023-08-27 19:12:14 +0200

    tests: Restructure, enable daemon tests on CI

--- tests/ci/build-and-test.sh
+++ tests/ci/build-and-test.sh
@@ -4,9 +4,12 @@ set -e
 if [ -d "build" ]; then
   rm build -rf
 fi
-meson build -Dlocal_checkout=true -Ddaemon_tests=false $@
+meson build \
+    -Dlocal_checkout=true \
+    -Ddaemon_tests=true \
+    $@
 
 # Build, Test & Install
 ninja -C build
 meson test -C build --print-errorlogs
-DEST=/tmp/install_root/ ninja -C build install
+DESTDIR=/tmp/install_root/ ninja -C build install
