174e0531d00fa571f3efbea71dbde88a9c31a314
Gleb Popov <6yearold@gmail.com>
2023-09-01 15:24:52 +0300

    Use portable shebang in shell scripts.
    This unbreaks most of tests on FreeBSD.

--- tests/data/pk-spawn-test.sh
+++ tests/data/pk-spawn-test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Copyright (C) 2007 Richard Hughes <richard@hughsie.com>
 # Licensed under the GNU General Public License Version 2
 # This program is free software; you can redistribute it and/or modify
