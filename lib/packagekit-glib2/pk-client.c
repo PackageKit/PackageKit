PackageKit/PackageKit

commit 44215db95d9cfda23045d9a9b97175519f473eb2

pk-client: Signal a transaction as finished if it disappears off the bus
The client code already does this if the `Finished` signal is received,
or if the packagekitd process completely disappears off the bus.
However, if the transaction is destroyed by the daemon without emitting
`Finished`, it’s possible for the client proxy for it to never complete.

Fix that by calling `pk_client_state_finish()` in a few additional
locations.

Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

lib/packagekit-glib2/pk-client.c
@@ -349,7 +349,12 @@ pk_client_cancellable_cancel_cb (GCancellable *cancellable,
 
 	/* dbus method has not yet fired */
 	if (state->proxy == NULL) {
+		g_autoptr(GError) local_error = NULL;
+
 		g_debug ("Cancelled, but no proxy, not sure what to do here");
+		local_error = g_error_new_literal (PK_CLIENT_ERROR, PK_CLIENT_ERROR_FAILED,
+						   "PackageKit transaction disappeared");
+		pk_client_state_finish (state, local_error);
 		return;
 	}
 
@@ -1582,8 +1587,16 @@ pk_client_signal_cb (GDBusProxy *proxy,
 		}
 		return;
 	}
-	if (g_strcmp0 (signal_name, "Destroy") == 0)
+	if (g_strcmp0 (signal_name, "Destroy") == 0) {
+		g_autoptr(GError) local_error = NULL;
+
+		if (state->waiting_for_finished)
+			local_error = g_error_new_literal (PK_CLIENT_ERROR, PK_CLIENT_ERROR_FAILED,
+							   "PackageKit transaction disappeared");
+
+		pk_client_state_finish (state, local_error);
 		return;
+	}
 }
 
 static void

commit cbb5a6d5ca849e4dca5a68b13ee655146996f002

pk-client: Fix runtime warning when backend does not support role
The daemon returns "op not supported by the backend" as part of the method
call response, but this response can be received after the "Destroy" signal,
which causes infinite wait for the transaction end and runtime warnings
about NULL error being passed to the g_task_return_error() on the client side.

This is a regression from commit https://github.com/PackageKit/PackageKit/commit/44215db95d9cfda23045d9a9b97175519f473eb2

lib/packagekit-glib2/pk-client.c
@@ -1616,11 +1616,11 @@ pk_client_signal_cb (GDBusProxy *proxy,
 	if (g_strcmp0 (signal_name, "Destroy") == 0) {
 		g_autoptr(GError) local_error = NULL;
 
-		if (state->waiting_for_finished)
+		if (state->waiting_for_finished) {
 			local_error = g_error_new_literal (PK_CLIENT_ERROR, PK_CLIENT_ERROR_FAILED,
 							   "PackageKit transaction disappeared");
-
-		pk_client_state_finish (state, g_steal_pointer (&local_error));
+			pk_client_state_finish (state, g_steal_pointer (&local_error));
+		}
 		return;
 	}
 }