8efa019d635fdc18b385db3eeb056cb34101715e
Corentin Noël <corentin.noel@collabora.com>
2025-05-13 21:20:03 +0200

    PkScheduler: Modernize the codebase

--- src/pk-scheduler.h
+++ src/pk-scheduler.h
@@ -30,32 +30,12 @@
 G_BEGIN_DECLS
 
 #define PK_TYPE_SCHEDULER		(pk_scheduler_get_type ())
-#define PK_SCHEDULER(o)			(G_TYPE_CHECK_INSTANCE_CAST ((o), PK_TYPE_SCHEDULER, PkScheduler))
-#define PK_SCHEDULER_CLASS(k)		(G_TYPE_CHECK_CLASS_CAST((k), PK_TYPE_SCHEDULER, PkSchedulerClass))
-#define PK_IS_SCHEDULER(o)	 	(G_TYPE_CHECK_INSTANCE_TYPE ((o), PK_TYPE_SCHEDULER))
-#define PK_IS_SCHEDULER_CLASS(k)	(G_TYPE_CHECK_CLASS_TYPE ((k), PK_TYPE_SCHEDULER))
-#define PK_SCHEDULER_GET_CLASS(o)	(G_TYPE_INSTANCE_GET_CLASS ((o), PK_TYPE_SCHEDULER, PkSchedulerClass))
+G_DECLARE_FINAL_TYPE (PkScheduler, pk_scheduler, PK, SCHEDULER, GObject)
+
 #define PK_SCHEDULER_ERROR		(pk_scheduler_error_quark ())
-#define PK_SCHEDULER_TYPE_ERROR		(pk_scheduler_error_get_type ())
+#define PK_SCHEDULER_TYPE_ERROR	(pk_scheduler_error_get_type ())
 
-typedef struct PkSchedulerPrivate PkSchedulerPrivate;
 
-typedef struct
-{
-	 GObject		 parent;
-	 PkSchedulerPrivate	*priv;
-} PkScheduler;
-
-typedef struct
-{
-	GObjectClass		 parent_class;
-} PkSchedulerClass;
-
-#ifdef G_DEFINE_AUTOPTR_CLEANUP_FUNC
-G_DEFINE_AUTOPTR_CLEANUP_FUNC(PkScheduler, g_object_unref)
-#endif
-
-GType		 pk_scheduler_get_type	  	(void);
 PkScheduler	*pk_scheduler_new		(GKeyFile	*conf);
 
 gboolean	 pk_scheduler_create		(PkScheduler	*scheduler,
