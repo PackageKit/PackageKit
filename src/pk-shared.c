4d52ff41db56de5f7d70187127d83c7c796826d0
Matthias Klumpp <matthias@tenstral.net>
2026-08-21 04:01:13 +0200

    trivial: Fix local config file path

--- src/pk-shared.c
+++ src/pk-shared.c
@@ -347,7 +347,7 @@ pk_util_get_config_filename (void)
 
 #if PK_BUILD_LOCAL
 	/* try a local path first */
-	path = g_build_filename ("..", "etc", "PackageKit.conf", NULL);
+	path = g_build_filename ("..", "data", "config", "PackageKit.conf", NULL);
 	if (g_file_test (path, G_FILE_TEST_EXISTS))
 		goto out;
 	g_debug ("local config file not found '%s'", path);
