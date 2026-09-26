69e1b2e60c6d5c5c429f4c1b9758f03f4bd9c192
Matthias Klumpp <matthias@tenstral.net>
2026-09-26 20:59:08 +0200

    Update license headers to more modern form with correct FSF address
    We should at some point maybe replace them with SPDX identifiers, but
    this was the most minimal change so far.

--- python/packagekit/misc.py
+++ python/packagekit/misc.py
@@ -2,9 +2,9 @@
 #
 # Licensed under the GNU General Public License Version 2
 #
-# This program is free software; you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation, either version 2 of the license, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -13,8 +13,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # (c) 2008
 #     Tim Lauridsen <timlau@fedoraproject.org>
